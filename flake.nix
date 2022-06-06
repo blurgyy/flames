7a003f2370646af2d00122268a91c171c1d13a45
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-06 09:06:05 +0800

    chore: bump inputs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -3,26 +3,12 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
     #nixpkgs.url = github:NixOS/nixpkgs;
-    nixos-cn = {
-      url = github:nixos-cn/flakes;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = github:nix-community/home-manager;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvfetcher = {
-      url = github:berberman/nvfetcher;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nbfc-linux = {
-      url = github:nbfc-linux/nbfc-linux;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = github:Mic92/sops-nix;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-cn = { url = github:nixos-cn/flakes; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = { url = github:nix-community/home-manager; inputs.nixpkgs.follows = "nixpkgs"; };
+    nvfetcher = { url = github:berberman/nvfetcher; inputs.nixpkgs.follows = "nixpkgs"; };
+    nbfc-linux = { url = github:nbfc-linux/nbfc-linux; inputs.nixpkgs.follows = "nixpkgs"; };
+    sops-nix = { url = github:Mic92/sops-nix; inputs.nixpkgs.follows = "nixpkgs"; };
+    acremote-backend = { url = gitlab:highsunz/acremote-backend; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   # `self` denotes this flake, otther function arguments are the flakes
