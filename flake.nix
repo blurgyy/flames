cdf4b7d7ebb46129825bb2a83beb64c198d34fa1
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-07 10:21:31 +0800

    chore: add `acremote` instead of `acremote-backend` as input
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "highsunz's flakes";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
+    flake-utils.url = github:numtide/flake-utils;
     #nixpkgs.url = github:NixOS/nixpkgs;
     nixos-cn = { url = github:nixos-cn/flakes; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = github:nix-community/home-manager; inputs.nixpkgs.follows = "nixpkgs"; };
     nvfetcher = { url = github:berberman/nvfetcher; inputs.nixpkgs.follows = "nixpkgs"; };
     nbfc-linux = { url = github:nbfc-linux/nbfc-linux; inputs.nixpkgs.follows = "nixpkgs"; };
     sops-nix = { url = github:Mic92/sops-nix; inputs.nixpkgs.follows = "nixpkgs"; };
-    acremote-backend = { url = gitlab:highsunz/acremote-backend; inputs.nixpkgs.follows = "nixpkgs"; };
+    acremote = { url = gitlab:highsunz/acremote; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   # `self` denotes this flake, otther function arguments are the flakes
