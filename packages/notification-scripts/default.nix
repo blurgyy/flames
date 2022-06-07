64f2fb11b91489ae1f9b7c3780b4e7611cac0158
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-07 21:10:27 +0800

    fix(packages): dash in env var names won't get substituted properly
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/notification-scripts/default.nix
+++ packages/notification-scripts/default.nix
@@ -6,12 +6,13 @@ stdenv.mkDerivation {
   phases = [ "installPhase" ];
 
   # NOTE: Need these for `substituteAllInPlace` to work
-  inherit bash light dunst slurp grim imagemagick wl-clipboard flameshot pamixer coreutils;
+  inherit bash light dunst slurp grim imagemagick flameshot pamixer coreutils;
 
   installPhase = ''
     install -Dt $out/bin -m555 $src/*
     for script in $out/bin/*; do
       substituteAllInPlace $script
+      substituteInPlace $script --replace @wl-clipboard@ ${wl-clipboard}
     done
   '';
 }
