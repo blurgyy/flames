618e63bc1c4b6d6830e2bc56752ed59715352199
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-09 21:08:14 +0800

    packages: bump lsp-format-nvim
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/lsp-format-nvim/default.nix
+++ packages/lsp-format-nvim/default.nix
@@ -1,12 +1,12 @@
 { prev, vimUtils, fetchFromGitHub }:
 vimUtils.buildVimPluginFrom2Nix rec {
   name = "lsp-format-nvim";
-  version = "v2.3.3";
+  version = "v2.4.1";
   src = fetchFromGitHub {
     owner = "lukas-reineke";
     repo = "lsp-format.nvim";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-LSmhW2gj+3W4KiFmqw2qrEyzVwugcRmTd/qPRZwRiQU=";
+    sha256 = "sha256-xFA+9JO3Rnj/CAYXb+oOnbslH3jgEapHA67I6dMFRFI=";
   };
   meta.homepage = "https://github.com/lukas-reineke/lsp-format.nvim";
 }
