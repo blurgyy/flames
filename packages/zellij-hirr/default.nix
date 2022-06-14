a91e0128ec5f7e08c3d84e26414ff8cfe054964b
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-14 19:51:51 +0800

    packages(zellij-hirr): update zellij-hirr's description
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/zellij-hirr/default.nix
+++ packages/zellij-hirr/default.nix
@@ -1,8 +1,8 @@
-{ zellij, fetchpatch }:
-zellij.overrideAttrs (oldAttrs: {
+{ zellij, fetchpatch }: zellij.overrideAttrs (oldAttrs: {
   pname = "zellij-hirr";
   patches = oldAttrs.patches ++ [
     ./0001-use-shorter-render_pause.patch
   ];
+  meta.description = oldAttrs.meta.description + " (with refresh rate boosted to 3ms/frame)";
   meta.mainProgram = "zellij";
 })
