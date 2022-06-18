4a14619753a50a0e5ee9d42b8cd2cefa63dd7334
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-19 02:03:29 +0800

    modules: make rathole package configurable
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- modules/rathole.nix
+++ modules/rathole.nix
@@ -3,6 +3,10 @@
 in {
   options.services.rathole = {
     enable = mkEnableOption "Reverse proxy with rathole";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.rathole;
+    };
     configFile = mkOption {
       type = with types; oneOf [ path string ];
     };
@@ -10,7 +14,7 @@ in {
   config = mkIf cfg.enable {
     systemd.services.rathole = with pkgs; {
       wantedBy = [ "multi-user.target" ];
-      script = ''${rathole}/bin/rathole ${config.services.rathole.configFile}'';
+      script = "${cfg.package}/bin/rathole ${config.services.rathole.configFile}";
       unitConfig = {
         Restart = "on-failure";
         RestartSec = 5;
