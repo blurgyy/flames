1228ac1244b422e3067efed03e4bd17cf5887c67
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-19 12:56:01 +0800

    fix(modules): Restart/RestartSec should be put in serviceConfig
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- modules/rathole.nix
+++ modules/rathole.nix
@@ -15,7 +15,7 @@ in {
     systemd.services.rathole = with pkgs; {
       wantedBy = [ "multi-user.target" ];
       script = "${cfg.package}/bin/rathole ${config.services.rathole.configFile}";
-      unitConfig = {
+      serviceConfig = {
         Restart = "on-failure";
         RestartSec = 5;
       };
