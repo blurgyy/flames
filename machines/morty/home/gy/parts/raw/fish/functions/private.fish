6ca11a6da7c480939ed15fea87e3c2a317cd2a02
Gaoyang Zhang <gy@blurgy.xyz>
2022-06-26 13:51:44 +0800

    style: update highlight color in fish function `private`
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/morty/home/gy/parts/raw/fish/functions/private.fish
+++ machines/morty/home/gy/parts/raw/fish/functions/private.fish
@@ -6,12 +6,12 @@ set -lx XDG_DATA_HOME "$TMPHOME/.local/share"
 set -lx XDG_STATE_HOME "$TMPHOME/.local/state"
 set -lx XDG_RUNTIME_DIR "$TMPHOME/.local/run"
 fish \
-  --init-command 'trap \'echo " '(set_color -ir red)'WARN'(set_color normal)' $TMPHOME will be nuked in 10 seconds" && sh -c "sleep 10 && rm -rf \$TMPHOME" &\' EXIT' \
+  --init-command 'trap \'echo " '(set_color -ir bryellow)'WARN'(set_color normal)' $TMPHOME will be nuked in 10 seconds" && sh -c "sleep 10 && rm -rf \$TMPHOME" &\' EXIT' \
   --init-command 'fish_vi_key_bindings || true' \
   --init-command '
     function fish_greeting
       echo " '(set_color -ir brgreen)'INFO'(set_color normal)' The new \$HOME is: '(set_color -iu)$HOME(set_color normal)'"
-      echo " '(set_color -ir red)'WARN'(set_color normal)' Everything in the new \$HOME will be nuked when you leave this shell!"
+      echo " '(set_color -ir bryellow)'WARN'(set_color normal)' Everything in the new \$HOME will be nuked when you leave this shell!"
     end
   ' \
   --private
