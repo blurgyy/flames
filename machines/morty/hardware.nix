a4ee1581e6e0100d2f04b293306e93f86e09736d
Gaoyang Zhang <gy@blurgy.xyz>
2022-07-09 01:14:42 +0800

    morty: use boot.tmpOnTmpfs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/morty/hardware.nix
+++ machines/morty/hardware.nix
@@ -1,33 +1,30 @@
-{ config, lib, pkgs, modulesPath, ... }:
-{
+{ config, lib, pkgs, modulesPath, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-  boot.kernel.sysctl = {
-    "net.core.default_qdisc" = "fq";
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "dev.i915.perf_stream_paranoid" = 0;
-    "kernel.sysrq" = 1;
-    "vm.swappiness" = 200;
-    "vm.vfs_cache_pressure" = 50;
-    "vm.dirty_background_ratio" = 5;
-    "vm.dirty_ratio" = 80;
+  boot = {
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+    kernel.sysctl = {
+      "net.core.default_qdisc" = "fq";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "dev.i915.perf_stream_paranoid" = 0;
+      "kernel.sysrq" = 1;
+      "vm.swappiness" = 200;
+      "vm.vfs_cache_pressure" = 50;
+      "vm.dirty_background_ratio" = 5;
+      "vm.dirty_ratio" = 80;
+    };
+    tmpOnTmpfs = true;
   };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos-root";
     fsType = "btrfs";
     options = [ "noatime" "compress-force=zstd:3" "discard=async" ];
-  };
-
-  fileSystems."/tmp" = {
-    fsType = "tmpfs";
-    options = [ "defaults" "size=2G" ];
   };
 
   fileSystems."/boot" = {
