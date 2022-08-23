5cec35f4d82af38310dcca0d5d89c05c451415ec
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-23 09:43:11 +0800

    tdesktop-megumifox: disable TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/tdesktop-megumifox/default.nix
+++ packages/tdesktop-megumifox/default.nix
@@ -1,6 +1,9 @@
 { tdesktop, fetchpatch, fetchurl }: tdesktop.overrideAttrs (oldAttrs: {
   pname = "tdesktop-megumifox";
   patchFlags = "-lNp1";  # See the `prepare()` function in the PKGBUILD from archlinuxcn: <https://github.com/archlinuxcn/repo/tree/master/archlinuxcn/telegram-desktop-megumifox/PKGBUILD>
+  cmakeFlags = oldAttrs.cmakeFlags ++ [
+    "-DTDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME=ON"
+  ];
   patches = [
   # NOTE: These patches are fetched and modified from <https://github.com/archlinuxcn/repo/tree/master/archlinuxcn/telegram-desktop-megumifox>
     ./0001-Use-QGuiApplication-font-instead-of-opensans.patch
