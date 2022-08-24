becd00b28a95456d7a79ec33e5e3efbad1f227fa
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-24 12:43:05 +0800

    cube: update network config
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/cube/network.nix
+++ machines/cube/network.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   networking.nftables.ruleset = builtins.readFile ../_parts/raw/nftables-default.conf;
   services.haproxy-tailored = {
     enable = true;
@@ -9,6 +9,24 @@
         binds = [ "*:80" ];
         alpns = [ "http/1.1" ];
         requestRules = [ "redirect scheme https code 301 unless { ssl_fc }" ];
+      }
+      {
+        name = "tls-in";
+        mode = "tcp";
+        binds = [ "*:443" ];
+        alpns = [ "h2" "http/1.1" ];
+        domain = {
+          name = "cube.blurgy.xyz";
+          acme = {
+            enable = true;
+            email = "gy@blurgy.xyz";
+            credentialsFile = config.sops.secrets.acme-credentials-file.path;
+          };
+        };
+        requestRules = [
+          "inspect-delay 5s"
+          "content accept if { req_ssl_hello_type 1 }"
+        ];
       }
     ];
   };
