cb076af136fe0fed5e474029235f07581aa3f014
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-24 10:02:52 +0800

    morty: minor clean up
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/morty/sops.nix
+++ machines/morty/sops.nix
@@ -1,5 +1,11 @@
 { config, ... }: let
   sops-key-file = "/var/lib/${config.networking.hostName}.age";
+  defaultSopsFile = ./secrets.yaml;
+  age = {
+    keyFile = sops-key-file;
+    sshKeyPaths = [ ];  # Do not import ssh keys
+  };
+  gnupg.sshKeyPaths = [ ];  # Do not import ssh keys  
 in {
   users.users = {
     root.passwordFile = config.sops.secrets."passwords/root".path;
@@ -20,12 +26,7 @@ in {
   };
 
   sops = {
-    defaultSopsFile = ./secrets.yaml;
-    age = {
-      keyFile = sops-key-file;
-      sshKeyPaths = [ ];  # Do not import ssh keys
-    };
-    gnupg.sshKeyPaths = [ ];  # Do not import ssh keys  
+    inherit defaultSopsFile age gnupg;
     secrets = {
       "passwords/root".neededForUsers = true;
       "passwords/gy".neededForUsers = true;
