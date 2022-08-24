a2124b06c9f015d807cc53a90e29c9764bc95d45
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-24 15:05:45 +0800

    rpi: update network config
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/rpi/network.nix
+++ machines/rpi/network.nix
@@ -15,53 +15,7 @@
 in {
   networking.nftables.ruleset = builtins.readFile ../_parts/raw/nftables-default.conf;
   services.v2ray-tailored = {
-    client = {
-      enable = true;
-      uuid = config.sops.placeholder."v2ray/id";
-      soMark = 27;
-      fwMark = 39283;
-      ports.http = config.sops.placeholder."v2ray/ports/http";
-      ports.socks = config.sops.placeholder."v2ray/ports/socks";
-      ports.tproxy = config.sops.placeholder."v2ray/ports/tproxy";
-      remotes = [
-        rec {
-          tag = "us-00";
-          address = config.sops.placeholder."v2ray/addresses/${tag}";
-          port = 443;
-          domain = config.sops.placeholder."v2ray/domains/${tag}";
-          wsPath = null;
-        }
-        rec {
-          tag = "eu-00";
-          address = config.sops.placeholder."v2ray/addresses/${tag}";
-          port = 443;
-          domain = config.sops.placeholder."v2ray/domains/${tag}";
-          wsPath = null;
-        }
-        rec {
-          tag = "cn-00";
-          address = config.sops.placeholder."v2ray/addresses/${tag}";
-          port = 443;
-          domain = config.sops.placeholder."v2ray/addresses/${tag}";
-          wsPath = null;
-          allowInsecure = true;
-        }
-        rec {
-          tag = "wss-us-00";
-          address = config.sops.placeholder."v2ray/domains/us-00";
-          port = 443;
-          domain = config.sops.placeholder."v2ray/domains/us-00";
-          wsPath = config.sops.placeholder."v2ray/ws-path";
-        }
-        rec {
-          tag = "wss-eu-00";
-          address = config.sops.placeholder."v2ray/domains/eu-00";
-          port = 443;
-          domain = config.sops.placeholder."v2ray/domains/eu-00";
-          wsPath = config.sops.placeholder."v2ray/ws-path";
-        }
-      ];
-    };
+    client = (import ../_parts/v2ray.nix { inherit config; }).client;
   };
   services.rathole = {
     enable = true;
--- machines/rpi/sops.nix
+++ machines/rpi/sops.nix
@@ -28,11 +28,13 @@ in {
       "v2ray/ws-path" = { };
       "v2ray/id" = { };
       "v2ray/domains/eu-00" = { };
+      "v2ray/domains/hk-00" = { };
       "v2ray/domains/us-00" = { };
       "v2ray/domains/wss-eu-00" = { };
       "v2ray/domains/wss-us-00" = { };
       "v2ray/addresses/cn-00" = { };
       "v2ray/addresses/eu-00" = { };
+      "v2ray/addresses/hk-00" = { };
       "v2ray/addresses/us-00" = { };
 
       "rathole/remote_addr" = { };
