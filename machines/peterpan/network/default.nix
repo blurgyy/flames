58b1dfe71ccfec1aecff628a771ace1b76f6101c
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-25 16:15:13 +0800

    peterpan: move network credentials from peterpan/sops.nix to peterpan/network
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- machines/peterpan/network/default.nix
+++ machines/peterpan/network/default.nix
@@ -1,8 +1,52 @@
-{ config, ... }: {
+{ config, ... }: let
+  ratholeServiceNames = [
+    "ssh-morty"
+    "ssh-rpi"
+    "ssh-watson"
+    "ssh-lab-2x1080ti"
+    "ssh-lab-shared"
+    "coderp-watson"
+    "acremote-rpi"
+  ];
+  ratholeServiceTokens = (builtins.listToAttrs (map (name: {
+    name = "rathole/${name}/token";
+    value = {};
+  }) ratholeServiceNames));
+  ratholeServiceAddrs = (builtins.listToAttrs (map (name: {
+    name = "rathole/${name}/addr";
+    value = {};
+  }) ratholeServiceNames));
+in {
   networking.nftables = {
     enable = true;
     ruleset = builtins.readFile ../../_parts/raw/nftables-default.conf;
   };
+  sops.secrets = {
+    "v2ray/ports/api" = {};
+    "v2ray/ports/tcp" = {};
+    "v2ray/ports/wss" = {};
+    "v2ray/ws-path" = {};
+
+    "v2ray/users/00/uuid" = {};
+    "v2ray/users/01/uuid" = {};
+    "v2ray/users/02/uuid" = {};
+    "v2ray/users/03/uuid" = {};
+    "v2ray/users/04/uuid" = {};
+    "v2ray/users/05/uuid" = {};
+
+    "v2ray/users/00/email" = {};
+    "v2ray/users/01/email" = {};
+    "v2ray/users/02/email" = {};
+    "v2ray/users/03/email" = {};
+    "v2ray/users/04/email" = {};
+    "v2ray/users/05/email" = {};
+
+    "v2ray/ports/reverse" = {};
+    "v2ray/users/reverse/uuid" = {};
+    "v2ray/users/reverse/email" = {};
+
+    "rathole/bind-addr" = {};
+  } // ratholeServiceTokens // ratholeServiceAddrs;
   services = {
     haproxy-tailored = import ./haproxy.nix { inherit config; };
     rathole = {
--- machines/peterpan/sops.nix
+++ machines/peterpan/sops.nix
@@ -1,62 +1,13 @@
 { config, ... }: let
   sops-key-file = "/var/lib/${config.networking.hostName}.age";
-  defaultSopsFile = ./secrets.yaml;
-  age = {
-    keyFile = sops-key-file;
-    sshKeyPaths = [ ];  # Do not import ssh keys
-  };
-  gnupg.sshKeyPaths = [ ];  # Do not import ssh keys  
 in {
   sops = {
-    inherit defaultSopsFile age gnupg;
-    secrets = {
-      "v2ray/ports/api" = {};
-      "v2ray/ports/tcp" = {};
-      "v2ray/ports/wss" = {};
-      "v2ray/ws-path" = {};
-
-      "v2ray/users/00/uuid" = {};
-      "v2ray/users/01/uuid" = {};
-      "v2ray/users/02/uuid" = {};
-      "v2ray/users/03/uuid" = {};
-      "v2ray/users/04/uuid" = {};
-      "v2ray/users/05/uuid" = {};
-
-      "v2ray/users/00/email" = {};
-      "v2ray/users/01/email" = {};
-      "v2ray/users/02/email" = {};
-      "v2ray/users/03/email" = {};
-      "v2ray/users/04/email" = {};
-      "v2ray/users/05/email" = {};
-
-      "v2ray/ports/reverse" = {};
-      "v2ray/users/reverse/uuid" = {};
-      "v2ray/users/reverse/email" = {};
-
-      "rathole/bind-addr" = {};
-    } // (builtins.listToAttrs (map (name: {
-      name = "rathole/${name}/token";
-      value = {};
-    }) [
-      "ssh-morty"
-      "ssh-rpi"
-      "ssh-watson"
-      "ssh-lab-2x1080ti"
-      "ssh-lab-shared"
-      "coderp-watson"
-      "acremote-rpi"
-    ])) // (builtins.listToAttrs (map (name: {
-      name = "rathole/${name}/addr";
-      value = {};
-    }) [
-      "ssh-morty"
-      "ssh-rpi"
-      "ssh-watson"
-      "ssh-lab-2x1080ti"
-      "ssh-lab-shared"
-      "coderp-watson"
-      "acremote-rpi"
-    ]));
+    defaultSopsFile = ./secrets.yaml;
+    age = {
+      keyFile = sops-key-file;
+      sshKeyPaths = [];  # Do not import ssh keys
+    };
+    gnupg.sshKeyPaths = [];
   };
   environment.variables.SOPS_AGE_KEY_FILE = sops-key-file;
 }
