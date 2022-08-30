e8c6644907bb1b44e382b0c993162b3b0ab34e2f
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-30 20:21:57 +0800

    hydra: set shell for "hydra-builder" user on peterpan
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/peterpan/services/hydra.nix
+++ nixos/peterpan/services/hydra.nix
@@ -1,8 +1,9 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   users.users.hydra-builder = {
     group = config.users.groups.hydra-builder.name;
     isSystemUser = true;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiW8FrX/Nfoqm7uocB7/OMPxmUxxKUMm8uU7ETm1J5r" ];
+    shell = pkgs.bash;
   };
   users.groups.hydra-builder = {};
   nix.settings.trusted-users = [ config.users.users.hydra-builder.name ];
