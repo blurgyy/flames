a948070b26b392a010c6c1bb197efc8fda94784c
Gaoyang Zhang <gy@blurgy.xyz>
2022-08-31 11:26:24 +0800

    hydra: update packages/release.nix
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/release.nix
+++ packages/release.nix
@@ -1,17 +1,12 @@
 { nixpkgs
-, useCross ? false
-, crossPlatform ? "x86_64-pc-linux-gnu"  # NOTE: see available names from <https://github.com/NixOS/nixpkgs/blob/master/lib/systems/examples.nix>
+, system
 , ignoredPkgs ? []
 }: let
-  lib = import <nixpkgs/lib>;
   nixpkgsArgs = {
-    system = "aarch64-linux";
+    inherit system;
     config.allowUnfree = true;
     config.inHydra = true;
     overlays = [ (import ./.).overlay ];
-  } // (if useCross
-    then { crossSystem.config = crossPlatform; }
-    else {}
-  );
+  };
   pkgs = import <nixpkgs> nixpkgsArgs;
 in builtins.removeAttrs ((import ./.).packages pkgs) ignoredPkgs
