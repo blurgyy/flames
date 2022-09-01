53ef1c926fd2e5a3527657e68da3c27d36a4af81
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-01 12:59:40 +0800

    cindy: enable wakapi
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/cindy/services/default.nix
+++ nixos/cindy/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hydra.nix
     ./vaultwarden
+    ./wakapi.nix
     ./webdav.nix
   ];
   sops.secrets.acme-credentials-file = { owner = config.users.users.haproxy.name; };
@@ -20,6 +21,7 @@
         { name = "hydra"; condition = "if is_hydra"; }
         { name = "cache"; condition = "if is_cache"; }
         { name = "webdav"; condition = "if is_webdav"; }
+        { name = "wakapi"; condition = "if is_wakapi"; }
       ];
     };
   };
---
+++ nixos/cindy/services/wakapi.nix
@@ -0,0 +1,43 @@
+{ config, ... }: let
+  wakapiDomain = "codingstats.${config.networking.domain}";
+in {
+  sops.secrets = {
+    "wakapi/salt" = {};
+    "wakapi/smtp/username" = {};
+    "wakapi/smtp/password" = {};
+  };
+  services = {
+    wakapi = {
+      enable = true;
+      server = {
+        addr = "127.0.0.1";
+        port = 31243;
+        publicUrl = "https://${wakapiDomain}";
+      };
+      security = {
+        passwordSalt = config.sops.placeholder."wakapi/salt";
+        allowSignup = true;
+        exposeMetrics = false;
+      };
+      smtp = {
+        enable = true;
+        sender = "Wakapi <notification-bot@${config.networking.domain}>";
+        host = config.networking.domain;
+        port = 587;
+        username = config.sops.placeholder."wakapi/smtp/username";
+        password = config.sops.placeholder."wakapi/smtp/password";
+        tls = false;
+      };
+    };
+    haproxy-tailored = {
+      frontends.tls-offload-front = {
+        acls = [ { name = "is_wakapi"; body = "hdr(host) -i ${wakapiDomain}"; } ];
+        domain.extraNames = [ wakapiDomain ];
+      };
+      backends.wakapi = {
+        mode = "http";
+        server.address = "127.0.0.1:${toString config.services.wakapi.server.port}";
+      };
+    };
+  };
+}
