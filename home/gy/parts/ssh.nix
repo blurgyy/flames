bfa449ba16e48dbf0d352a1d8ad5fbc937a2e2b4
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-03 12:30:09 +0800

    home: use root as default ssh user on rpi
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/ssh.nix
+++ home/gy/parts/ssh.nix
@@ -30,7 +30,7 @@
         { host.port = 9091; bind.port = 49091; host.address = "localhost"; }
       ];
     };
-    pi = { hostname = relay; port = 10013; };
+    pi = { hostname = relay; user = "root"; port = 10013; };
 
     hy = { hostname = "10.76.2.98"; user = "haoyu"; };
     glab = { hostname = "10.76.2.83"; user = "git"; port = 9962; };
