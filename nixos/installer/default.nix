a6a950632599084c9322503ea88d0f9f7f1c30b0
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-10 21:31:02 +0800

    installer: simplify
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/installer/default.nix
+++ nixos/installer/default.nix
@@ -1,12 +1,6 @@
 { system, self, nixpkgs, inputs }: nixpkgs.lib.nixosSystem {
   inherit system;
-  modules = (import ../_parts/defaults {
-    inherit system self inputs;
-    headless = true;
-    isQemuGuest = true;
-    withSecrets = false;
-    withBinfmtEmulation = false;
-  }) ++ [
+  modules = [
     ./configuration.nix
     inputs.nixos-generators.nixosModules.kexec  # nix build .#nixosConfigurations.${config.networking.hostName}.config.system.build.kexec_tarball --impure
     { kexec.autoReboot = false; }
