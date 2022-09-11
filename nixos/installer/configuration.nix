90119d65f5dde1227574dd5916a2c83499460a0d
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-11 13:55:22 +0800

    installer: minimize more
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/installer/configuration.nix
+++ nixos/installer/configuration.nix
@@ -1,7 +1,22 @@
-{ lib, pkgs, ... }: {
-  users.users.root.openssh.authorizedKeys.keys = import ../_parts/defaults/public-keys.nix;
+{ lib, pkgs, modulesPath, ... }: {
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+    "${modulesPath}/profiles/qemu-guest.nix"
+  ];
 
-  networking.hostName = "installer";
+  boot = {
+    kernelPackages = pkgs.linuxPackages;
+    supportedFilesystems = [ "btrfs" ];
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = import ../_parts/defaults/public-keys.nix;
+  services.openssh.enable = true;
+
+  networking = {
+    hostName = "installer";
+    useNetworkd = true;
+    firewall.enable = false;
+  };
   i18n.defaultLocale = "en_US.UTF-8";
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "eurosign:e,caps:escape";
