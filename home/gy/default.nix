b2e6de7f82ab0d017a1bdd291e943ad005354b1e
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-19 22:23:07 +0800

    home: remove overlay for gitsigns-nvim
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/default.nix
+++ home/gy/default.nix
@@ -10,14 +10,14 @@
         inherit (import inputs.nixpkgs-difftastic-terminal-width-fix { inherit system; }) difftastic;
         inherit (inputs.neovim.packages.${system}) neovim;
 	vimPlugins = prev.vimPlugins.extend (finalPlugins: prevPlugins: {
-	  gitsigns-nvim = prevPlugins.gitsigns-nvim.overrideAttrs (o: {
-	    src = prev.fetchFromGitHub {
-	      owner = "lewis6991";
-	      repo = "gitsigns.nvim";
-	      rev = "refs/tags/v0.5";
-	      sha256 = "sha256-kyiQoboYq4iNLOj1iKA2cfXQ9FFiRYdvf55bX5Xvj8A=";
-	    };
-	  });
+	  #gitsigns-nvim = prevPlugins.gitsigns-nvim.overrideAttrs (o: {
+	  #  src = prev.fetchFromGitHub {
+	  #    owner = "lewis6991";
+	  #    repo = "gitsigns.nvim";
+	  #    rev = "refs/tags/v0.5";
+	  #    sha256 = "sha256-kyiQoboYq4iNLOj1iKA2cfXQ9FFiRYdvf55bX5Xvj8A=";
+	  #  };
+	  #});
 	});
       })
     ];
