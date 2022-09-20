63d2c41836fc9057bb6fc35847311ba4598d64ce
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-20 23:52:58 +0800

    nixos: enable fcitx5 system-wise for non-headless machines
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/non-headless-extras.nix
+++ nixos/_parts/defaults/non-headless-extras.nix
@@ -29,6 +29,18 @@
   # Needed for swaylock to work
   security.pam.services.swaylock = { };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-gtk
+      fcitx5-lua
+      libsForQt5.fcitx5-qt
+      fcitx5-sogou-themes
+      fcitx5-fluent-dark-theme
+    ];
+  };
+
   fonts = let
     fontConfsRoot = ../raw/fontconfig;
   in {
