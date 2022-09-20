7643423f02171627a14c6d27a7535baebd31a23f
Gaoyang Zhang <gy@blurgy.xyz>
2022-09-20 11:55:39 +0800

    nixos: update default proxy configs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/vclient.nix
+++ nixos/_parts/vclient.nix
@@ -8,6 +8,7 @@
     "v2ray/id" = {};
     "v2ray/domains/eu-00" = {};
     "v2ray/domains/hk-00" = {};
+    "v2ray/domains/jp-00" = {};
     "v2ray/domains/us-00" = {};
     "v2ray/domains/us-01" = {};
     "v2ray/domains/us-02" = {};
@@ -16,6 +17,7 @@
     "v2ray/addresses/cn-00" = {};
     "v2ray/addresses/eu-00" = {};
     "v2ray/addresses/hk-00" = {};
+    "v2ray/addresses/jp-00" = {};
     "v2ray/addresses/us-00" = {};
     "v2ray/addresses/us-01" = {};
     "v2ray/addresses/us-02" = {};
@@ -53,6 +55,13 @@
       }
       rec {
         tag = "hk-00";
+        address = config.sops.placeholder."v2ray/addresses/${tag}";
+        port = 443;
+        domain = config.sops.placeholder."v2ray/domains/${tag}";
+        wsPath = null;
+      }
+      rec {
+        tag = "jp-00";
         address = config.sops.placeholder."v2ray/addresses/${tag}";
         port = 443;
         domain = config.sops.placeholder."v2ray/domains/${tag}";
