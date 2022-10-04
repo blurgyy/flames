ee303bec942c98e7db5748b2b89219fbf9df62cd
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-04 20:00:02 +0800

    chore: move expressions for hydraJobs to a separate file
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -46,40 +46,7 @@
       program = "${gpustat-wrapped}/bin/gpustat";
     };
   }) // {
-    hydraJobs = with builtins; let
-      allPackages = let
-        builtSystems = [ "aarch64-linux" "x86_64-linux" ];
-        preserveOnlyFrom = keys: attrset: foldl' (lhs: rhs: lhs // rhs) {} (map (key: { "${key}" = attrset.${key}; }) keys);
-        preserveOnlyAmong = keys: attrsets: foldl'
-          (lhs: rhs: listToAttrs (map
-            (key: {
-              name = key;
-              value = lhs.${key} or {} // rhs.${key} or {};
-            })
-            keys))
-          {}
-          (map (attrset: preserveOnlyFrom keys attrset) attrsets);
-      in preserveOnlyAmong builtSystems [
-        self.packages
-        (mapAttrs (sys: pkg: { nvfetcher = pkg; }) inputs.nvfetcher.defaultPackage)
-      ];
-      builtPackages = mapAttrs
-        (sys: pkgset: my.filterAttrs (name: pkg: !hasAttr "platforms" pkg.meta || elem sys pkg.meta.platforms) pkgset)
-        allPackages;
-      allNixosConfigurations = listToAttrs (attrValues (mapAttrs
-        (name: _: {
-          inherit name;
-          value = self.homeConfigurations.${name}.activationPackage;
-        }) self.homeConfigurations
-      ));
-      allHomeConfigurations = listToAttrs (attrValues (mapAttrs
-        (name: _: {
-          inherit name;
-          value = self.nixosConfigurations.${name}.config.system.build.toplevel;
-        }) self.nixosConfigurations
-      ));
-    in builtPackages // allNixosConfigurations // allHomeConfigurations;
-  } // {
+    hydraJobs = import ./outputs/jobs.nix { inherit inputs self my; };
     nixosModules = import ./modules;
     overlays.default = my.overlay;
     homeConfigurations = import ./outputs/home.nix { inherit nixpkgs inputs self; };
---
+++ outputs/jobs.nix
@@ -0,0 +1,33 @@
+{ inputs, self, my }: with builtins; let
+  allPackages = let
+    builtSystems = [ "aarch64-linux" "x86_64-linux" ];
+    preserveOnlyFrom = keys: attrset: foldl' (lhs: rhs: lhs // rhs) {} (map (key: { "${key}" = attrset.${key}; }) keys);
+    preserveOnlyAmong = keys: attrsets: foldl'
+      (lhs: rhs: listToAttrs (map
+        (key: {
+          name = key;
+          value = lhs.${key} or {} // rhs.${key} or {};
+        })
+        keys))
+      {}
+      (map (attrset: preserveOnlyFrom keys attrset) attrsets);
+  in preserveOnlyAmong builtSystems [
+    self.packages
+    (mapAttrs (sys: pkg: { nvfetcher = pkg; }) inputs.nvfetcher.defaultPackage)
+  ];
+  builtPackages = mapAttrs
+    (sys: pkgset: my.filterAttrs (name: pkg: !hasAttr "platforms" pkg.meta || elem sys pkg.meta.platforms) pkgset)
+    allPackages;
+  allNixosConfigurations = listToAttrs (attrValues (mapAttrs
+    (name: _: {
+      inherit name;
+      value = self.homeConfigurations.${name}.activationPackage;
+    }) self.homeConfigurations
+  ));
+  allHomeConfigurations = listToAttrs (attrValues (mapAttrs
+    (name: _: {
+      inherit name;
+      value = self.nixosConfigurations.${name}.config.system.build.toplevel;
+    }) self.nixosConfigurations
+  ));
+in builtPackages // allNixosConfigurations // allHomeConfigurations
