570d527af73f51aac5db49c1225c63ad719f3618
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-04 18:18:51 +0800

    v2ray-loyalsoldier: simplify package expression
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/v2ray-loyalsoldier/default.nix
+++ packages/v2ray-loyalsoldier/default.nix
@@ -3,10 +3,5 @@
 , loyalsoldier-geodata
 }: v2ray.overrideAttrs (oldAttrs: {
   pname = "v2ray-loyalsoldier";
-  buildCommand = ''
-    for file in ${v2ray}/bin/*; do
-      makeWrapper "$file" "$out/bin/$(basename "$file")" \
-        --suffix XDG_DATA_DIRS : ${loyalsoldier-geodata}/share
-    done
-  '';
+  assetsDrv = loyalsoldier-geodata;
 })
