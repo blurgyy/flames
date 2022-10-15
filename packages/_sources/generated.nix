ef692fe7547ae9c3384f3e5ff103b824f4db15a8
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-15 23:48:58 +0800

    Update loyalsoldier-geoip: 202210132216 → 202210142213 loyalsoldier-geosite: 202210132216 → 202210142213

--- packages/_sources/generated.nix
+++ packages/_sources/generated.nix
@@ -127,18 +127,18 @@
   };
   loyalsoldier-geoip = {
     pname = "loyalsoldier-geoip";
-    version = "202210132216";
+    version = "202210142213";
     src = fetchurl {
-      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202210132216/geoip.dat";
+      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202210142213/geoip.dat";
       sha256 = "sha256-BIebPJuRtIfBhc1AP+1BGC6vEPcP5Kvy1I6zgyRiWTg=";
     };
   };
   loyalsoldier-geosite = {
     pname = "loyalsoldier-geosite";
-    version = "202210132216";
+    version = "202210142213";
     src = fetchurl {
-      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202210132216/geosite.dat";
-      sha256 = "sha256-7PQOjcbY4GVG/hZ178dSX1hO2S/5c4OI+Dpaw/gemy4=";
+      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202210142213/geosite.dat";
+      sha256 = "sha256-1CLNCjdIkmBUU8NuOr9rCrvbh4SqO0Kh/A2AClZ8LfQ=";
     };
   };
   luarock-dbus_proxy = {
