fd148895d21572fc2fe474b7bd57972afb21a97d
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-18 16:37:55 +0800

    home: don't install meshlab in headless profiles
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/fish/default.nix
+++ home/gy/parts/fish/default.nix
@@ -48,7 +48,7 @@
     bsys = "nixos-rebuild build --use-remote-sudo -L -v --flake";
     shome = "${home-manager}/bin/home-manager switch -v --flake";
     ssys = "nixos-rebuild switch --use-remote-sudo -L -v --flake";
-    meshlab = "QT_QPA_PLATFORM=xcb ${sdwrap}/bin/sdwrap ${meshlab}/bin/meshlab";
+    meshlab = "QT_QPA_PLATFORM=xcb ${sdwrap}/bin/sdwrap meshlab";
   };
   plugins = [{
     name = "tide";
