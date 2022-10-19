09b6369af2fa85a55483d91f666e83d03c98ce6c
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-19 08:53:44 +0800

    home: show hostname at beginning of title in window module
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/waybar/default.nix
+++ home/gy/parts/waybar/default.nix
@@ -55,8 +55,8 @@
     "sway/window" = {
       icon = false;
       rewrite = {
-        "^\\[(\\S+)(\\s?.*?)\\s+@ (/.*?)\\] @ ([\\w-_\\.]+)$" = "<span foreground='${themeColor "yellow"}'>[</span><span foreground='${themeColor "blue"}'>$1</span>$2 @ <span underline='single'>$3</span><span foreground='${themeColor "yellow"}'>]</span> @ <span weight='bold' foreground='${themeColor "green"}'>$4</span>";
-        "^Zellij \\(.*@(.*)\\) - (\\S+)(\\s?.*?)\\s+@ (/.*?)$" = "<span foreground='${themeColor "yellow"}'>[</span><span foreground='${themeColor "blue"}'>$2</span>$3 @ <span underline='single'>$4</span><span foreground='${themeColor "yellow"}'>]</span> @ <span weight='bold' foreground='${themeColor "green"}'>$1</span>";
+        "^\\[(\\S+)(\\s?.*?)\\s+@ (/.*?)\\] @ ([\\w-_\\.]+)$" = "<span weight='bold' foreground='${themeColor "green"}'>$4</span>:<span foreground='${themeColor "yellow"}'>[</span><span foreground='${themeColor "blue"}'>$1</span>$2 @ <span underline='single'>$3</span><span foreground='${themeColor "yellow"}'>]</span>";
+        "^Zellij \\(.*@(.*)\\) - (\\S+)(\\s?.*?)\\s+@ (/.*?)$" = "<span weight='bold' foreground='${themeColor "green"}'>$1</span><span foreground='${themeColor "yellow"}'>[</span><span foreground='${themeColor "blue"}'>$2</span>$3 @ <span underline='single'>$4</span><span foreground='${themeColor "yellow"}'>]</span>";
         "^/home/gy/Zotero/storage/[A-Z0-9]{8}/(.*)$" = "$1";
         "(.*) .*? Mozilla Firefox$" = "$1";
       };
