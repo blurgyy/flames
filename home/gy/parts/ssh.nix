541940de345b8d3c43db825d311fdd0115f8b254
Gaoyang Zhang <gy@blurgy.xyz>
2022-10-28 19:52:59 +0800

    home: use hostname as names of ssh hosts
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/ssh.nix
+++ home/gy/parts/ssh.nix
@@ -10,17 +10,10 @@
   ];
   matchBlocks = let 
     peterpan = "81.69.28.75";
-    cindy = "130.61.57.3";
-    cube = "45.78.17.205";
-    hooper = "64.64.244.30";
-    quad = "45.139.193.21";
-    rubik = "193.32.151.152";
-    trigo = "154.9.139.26";
-    tetra = "154.12.36.120";
     relay = peterpan;
 
-    m = { hostname = relay; port = 10021; };
-    w = {
+    morty-relay = { hostname = relay; port = 10021; };
+    watson-relay = {
       hostname = relay;
       port = 10020;
       localForwards = [
@@ -29,39 +22,32 @@
         { host.port = 9091; bind.port = 49091; host.address = "localhost"; }
       ];
     };
-    pi = { hostname = relay; port = 10013; };
-  in {
-    inherit m w pi;
-    morty = m;
-    watson = w;
-    rpi = pi;
+    rpi-relay = { hostname = relay; port = 10013; };
+    "2x1080ti-relay" = { hostname = relay; port = 10023; };
+    shared-relay = { hostname = relay; port = 10025; };
 
-    pp = { hostname = peterpan; };
-    hooper = { hostname = hooper; };
-    cindy = { hostname = cindy; };
-    cube = { hostname = cube; };
-    quad = { hostname = quad; };
-    rubik = { hostname = rubik; };
-    trigo = { hostname = trigo; };
-    tetra = { hostname = tetra; };
+    applyHostname = hostnames: map (hostname: { ${hostname} = { inherit hostname; }; }) hostnames;
+  in (builtins.foldl' (x: y: x // y) {} (applyHostname [
+    "2x1080ti"
+    "cindy"
+    "cube"
+    "hooper"
+    "morty"
+    "peterpan"
+    "quad"
+    "rpi"
+    "rubik"
+    "shared"
+    "trigo"
+    "watson"
+  ])) // {
+    inherit morty-relay watson-relay rpi-relay "2x1080ti-relay" shared-relay;
 
-    gpp = { hostname = peterpan; port = 77; };
-    ghooper = { hostname = hooper; user = "git"; };
+    glab = { hostname = "10.76.2.83"; user = "git"; port = 9962; };
+    gpp = { hostname = "peterpan"; port = 77; };
+    ghooper = { hostname = "hooper"; user = "git"; };
 
     hy = { hostname = "10.76.2.98"; user = "haoyu"; };
-    glab = { hostname = "10.76.2.83"; user = "git"; port = 9962; };
-    "2x1080ti" = if name == "gy@watson" || name == "gy@cadliu" || name == "gy@cad-liu" then {
-      hostname = "192.168.1.22";
-    } else {
-      hostname = relay;
-      port = 10023;
-    };
-    shared = if name == "gy@watson" || name == "gy@cadliu" || name == "gy@cad-liu" then {
-      hostname =  "192.168.1.23";
-    } else {
-      hostname = relay;
-      port = 10025;
-    };
 
     github = { hostname = "github.com"; user = "git"; };
     aur = { hostname = "aur.archlinux.org"; user = "aur"; };
