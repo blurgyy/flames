045850dd58c95af6bba8e9c98f3a4483a8121112
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-02 21:09:01 +0800

    Revert "morty: hard-code IP address on wlan"
    This reverts commit 3b3202ec3140fa9d2c2f2936f27840251d66ffe7.
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/morty/network/wlan.nix
+++ nixos/morty/network/wlan.nix
@@ -13,14 +13,4 @@
       "ZJUWLAN-NEW".authProtocols = [ "NONE" ];
     };
   };
-  networking = {
-    defaultGateway = "192.168.0.1";
-    interfaces.wlan0 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.0.169";
-        prefixLength = 24;
-      }];
-    };
-  };
 }
