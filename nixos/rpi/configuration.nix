9f88050aa2251018f699cc483bf32cfaca7aadeb
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-03 15:29:05 +0800

    rpi: use latest kernel
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/rpi/configuration.nix
+++ nixos/rpi/configuration.nix
@@ -5,7 +5,6 @@
   imports = [ (modulesPath + "/installer/sd-card/sd-image-aarch64.nix") ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4;
     initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
     # ttyAMA0 is the serial console broken out to the GPIO
     kernelParams = [
