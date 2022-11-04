8af63961670127f6932fe300d906877e3b8cc068
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-05 00:44:31 +0800

    refactor: use lib.optionalAttrs instead of if-else expressions for including shared configs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/default.nix
+++ nixos/_parts/defaults/default.nix
@@ -4,7 +4,9 @@
 , isQemuGuest
 , withSecrets ? true
 , withBinfmtEmulation ? false
-}: [
+}: let
+  includeIf = inputs.nixpkgs.lib.optionalAttrs;
+in [
   ./configuration.nix
   ./hardware.nix
   ./network
@@ -17,33 +19,29 @@
       inputs.nixos-cn.overlay
     ];
   }
+
+  (includeIf withSecrets ./secret)
+  (includeIf withSecrets inputs.sops-nix.nixosModules.sops)
+
+  (includeIf (!headless) ./headful.nix)
+
+  (includeIf isQemuGuest ({ lib, pkgs, modulesPath, ... }: {
+    imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages;
+    documentation.nixos.enable = lib.mkDefault false;
+  }))
+  (includeIf (!isQemuGuest) ({ lib, pkgs, modulesPath, ... }: {
+    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    documentation.nixos.enable = lib.mkDefault true;
+  }))
+
+  (includeIf withBinfmtEmulation ({ lib, pkgs, ... }: {
+    boot.binfmt.emulatedSystems = with lib; let
+      getEmulation = system: {
+        "aarch64-linux" = [ "x86_64-linux" ];
+        "x86_64-linux" = [ "aarch64-linux" "i686-linux" ];
+      }.${pkgs.system};
+    in mkDefault (getEmulation pkgs.system);
+  }))
 ]
-
-++ (if withSecrets then [
-  ./secret
-  inputs.sops-nix.nixosModules.sops
-] else [])
-
-++ (if (!headless) then [
-  ./headful.nix
-] else [])
-
-++ (if isQemuGuest then [({ lib, pkgs, modulesPath, ... }: {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages;
-  documentation.nixos.enable = lib.mkDefault false;
-})] else [({ lib, pkgs, modulesPath, ... }: {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-  documentation.nixos.enable = lib.mkDefault true;
-})])
-
-++ (if withBinfmtEmulation then [({ lib, pkgs, ... }: {
-  boot.binfmt.emulatedSystems = with lib; let
-    getEmulation = system: {
-      "aarch64-linux" = [ "x86_64-linux" ];
-      "x86_64-linux" = [ "aarch64-linux" "i686-linux" ];
-    }.${pkgs.system};
-  in
-    mkDefault (getEmulation pkgs.system);
-})] else [])
