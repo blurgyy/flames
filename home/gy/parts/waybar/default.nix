adcb25b948a5b5acad0242dae52c9eff70c2e18a
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-12 22:54:37 +0800

    home: use unreleased padding fix from waybar's master branch
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/waybar/default.nix
+++ home/gy/parts/waybar/default.nix
@@ -1,6 +1,13 @@
 { pkgs, callWithHelpers }: {
   enable = true;
-  package = pkgs.waybar;
+  package = pkgs.waybar.overrideAttrs (o: {
+    src = pkgs.fetchFromGitHub {
+      owner = "Alexays";
+      repo = "Waybar";
+      rev = "master";
+      sha256 = "sha256-sR7RAVCODYtQsQzL0lxs0SbH+yZiKvE/TApuDsYM/Bw=";
+    };
+  });
   settings = callWithHelpers ./settings.nix {};
   style = callWithHelpers ./style.css.nix {};
   systemd = {
