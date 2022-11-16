e01741bf54c0b98ea62a39c4768e0f910948d4a0
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-16 09:20:36 +0800

    nixos: add official minecraft launcher to gaming.nix
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/gaming.nix
+++ nixos/_parts/defaults/gaming.nix
@@ -1,5 +1,6 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.steam.enable = true;
+  environment.systemPackages = with pkgs; [ minecraft ];
   networking.firewall-tailored = {
     acceptedPorts = [{
       port = 27036;
