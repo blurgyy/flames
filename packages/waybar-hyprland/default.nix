72ad59f6039efce6842139abf655be21a87aa16e
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-16 09:40:07 +0800

    waybar-hyprland: rename package
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/waybar-hyprland/default.nix
+++ packages/waybar-hyprland/default.nix
@@ -1,4 +1,5 @@
 { waybar, hyprland }: waybar.overrideAttrs (o: {
+  pname = o.pname + "-hyprland";
   postPatch = ''
     # REF: <https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#waybar>
     sed -Ee 's|zext_workspace_handle_v1_activate\(workspace_handle_\);|const std::string command = "${hyprland}/bin/hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());|g' -i src/modules/wlr/workspace_manager.cpp
