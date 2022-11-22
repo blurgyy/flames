b116d426618f1526712bb0ffebc1fa3e991c5740
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-22 22:02:28 +0800

    waybar-hyprland: do not hard code hyprland's out path into code
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/waybar-hyprland/default.nix
+++ packages/waybar-hyprland/default.nix
@@ -1,8 +1,9 @@
-{ waybar, hyprland }: waybar.overrideAttrs (o: {
+{ waybar }: waybar.overrideAttrs (o: {
   pname = o.pname + "-hyprland";
   postPatch = ''
     # REF: <https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#waybar>
-    sed -Ee 's|zext_workspace_handle_v1_activate\(workspace_handle_\);|const std::string command = "${hyprland}/bin/hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());|g' -i src/modules/wlr/workspace_manager.cpp
+    # NOTE: Remember to add hyprland's `bin` to waybar's runtime PATH
+    sed -Ee 's|zext_workspace_handle_v1_activate\(workspace_handle_\);|const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());|g' -i src/modules/wlr/workspace_manager.cpp
   '';
   mesonFlags = o.mesonFlags ++ [ "-Dexperimental=true" ];
 })
