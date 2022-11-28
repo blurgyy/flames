547b620bf90738d88f5bc343d33f32dc12f6080a
Gaoyang Zhang <gy@blurgy.xyz>
2022-11-28 23:29:36 +0800

    Revert "nixos: use linux-zen for headful machines"
    This reverts commit 2b0f76078fbf1ff4490b8f28e1989ab78a9e5508.
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/default.nix
+++ nixos/_parts/defaults/default.nix
@@ -33,9 +33,7 @@ in [
   }))
   (includeIf (!isQemuGuest) ({ lib, pkgs, modulesPath, ... }: {
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-    boot.kernelPackages = lib.mkDefault (if headless
-      then pkgs.linuxPackages_latest
-      else pkgs.linuxPackages_zen);
+    boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
     documentation.nixos.enable = lib.mkDefault true;
     environment.systemPackages = with pkgs; [ man-pages man-pages-posix ];
   }))
