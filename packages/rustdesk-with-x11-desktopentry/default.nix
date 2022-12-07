d115acf414f8983ef65aae3e31857dbc05ac818e
Gaoyang Zhang <gy@blurgy.xyz>
2022-12-08 01:54:21 +0800

    rustdesk-with-x11-desktopentry: avoid rebuilding of rustdesk
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/rustdesk-with-x11-desktopentry/default.nix
+++ packages/rustdesk-with-x11-desktopentry/default.nix
@@ -1,11 +1,20 @@
-{ rustdesk, makeDesktopItem }: rustdesk.overrideAttrs (o: {
-  desktopItems = o.desktopItems ++ [ (makeDesktopItem {
+{ stdenv, rustdesk, makeDesktopItem }: stdenv.mkDerivation rec {
+  pname = "rustdesk-with-x11-desktopentry";
+  inherit (rustdesk) version meta;
+  desktopItems = [(makeDesktopItem {
     name = "rustdesk-x11";
-    exec = ''env -u WAYLAND_DISPLAY ${o.meta.mainProgram}'';
+    exec = ''env -u WAYLAND_DISPLAY ${rustdesk}/bin/${rustdesk.meta.mainProgram}'';
     icon = "rustdesk";
     desktopName = "RustDesk (X11)";
-    comment = o.meta.description;
+    comment = rustdesk.meta.description;
     genericName = "Remote Desktop";
     categories = ["Network"];
   })];
-})
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir $out/share/applications -p
+    for desktop in ${toString desktopItems}; do
+      cp $desktop/share/applications/* $out/share/applications
+    done
+  '';
+}
