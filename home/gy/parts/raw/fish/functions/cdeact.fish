0290f07a66024f61b6ea770879ba87d0b5cdd180
Gaoyang Zhang <gy@blurgy.xyz>
2022-12-17 23:56:37 +0800

    home: do not unset _CE_CONDA
    REF: <https://github.com/conda/conda/blob/6d9415be6bb68daebb6f61d044801b1fa09ac160/tests/test_activate.py#L2367-L2369>
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/raw/fish/functions/cdeact.fish
+++ home/gy/parts/raw/fish/functions/cdeact.fish
@@ -3,6 +3,6 @@ if not set -l _conda_bin (__find_conda_bin)
 end
 $_conda_bin shell.fish deactivate $argv | source
 if test "$pipestatus[1]" -eq 0
-  set -e CONDA_EXE CONDA_SHLVL CONDA_PYTHON_EXE _CE_CONDA
+  set -e CONDA_EXE CONDA_SHLVL CONDA_PYTHON_EXE
   set -gx PIP_REQUIRE_VIRTUALENV 1
 end
