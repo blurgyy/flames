1a30f256b302283844724efe06a0aff52a1e1ada
Gaoyang Zhang <gy@blurgy.xyz>
2022-12-22 09:24:16 +0800

    home: autostart fcitx5 on user login via `xdg-desktop-autostart.target`
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/headful.nix
+++ home/gy/headful.nix
@@ -131,9 +131,9 @@
       ];
     in {
       sway-session.Unit.Wants = wm-session-wants;
-    } // (lib.optionalAttrs config.wayland.windowManager.hyprland.enable {
-      hyprland-session.Unit.Wants = wm-session-wants;
-    });
+      hyprland-session.Unit.Wants = lib.mkIf config.wayland.windowManager.hyprland.enable wm-session-wants;
+      xdg-desktop-autostart.Install.WantedBy = [ "default.target" ];
+    };
     services.flameshot = {
       Service.ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/Pictures/screenshots";
     };
@@ -151,6 +151,7 @@
       Install.WantedBy = [ "graphical-session.target" ]
         ++ (lib.optional config.wayland.windowManager.hyprland.enable "hyprland-session.target");
     };
+    services.fcitx5-daemon.Install.WantedBy = lib.mkForce [];
   };
 
   services = {
