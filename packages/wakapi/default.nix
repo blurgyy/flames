013a2eb3582268a79c02980fb94b5e18afbebbef
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-04 10:45:28 +0800

    wakapi: only build root package
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/wakapi/default.nix
+++ packages/wakapi/default.nix
@@ -2,6 +2,8 @@
   inherit (source) pname version src;
   vendorSha256 = "sha256-PCqJhfm3Rif0TPMkHAETNcv/r80qrT8c4AH9Cijkw5I=";
 
+  subPackages = [ "." ];
+
   meta = {
     homepage = "https://github.com/muety/wakapi";
     description = "A minimalist, self-hosted WakaTime-compatible backend for coding statistics ";
