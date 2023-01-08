233e02839833da3374e1498cab9d5ca1dea80981
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-08 11:19:13 +0800

    network: do not use proxy for {imap,mail}.zju.edu.cn
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/v2ray-tailored/client/routing/rules/20-direct-zju.nix
+++ nixos/_modules/v2ray-tailored/client/routing/rules/20-direct-zju.nix
@@ -1,10 +1,12 @@
 {
   domain = [
+    "domain:imap.zju.edu.cn"
+    "domain:mail.zju.edu.cn"
+    "domain:mirrors.zju.edu.cn"
     "domain:net.zju.edu.cn"
     "domain:net1.zju.edu.cn"
     "domain:net2.zju.edu.cn"
     "domain:net3.zju.edu.cn"
-    "domain:mirrors.zju.edu.cn"
     "domian:tracker.nexushd.org"
   ];
   outboundTag = "direct-zju";
