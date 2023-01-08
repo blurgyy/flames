8cf1576715a1353340fc4404b5c7d614c277b075
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-08 11:41:24 +0800

    style: minor update
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/v2ray-tailored/client/dns.nix
+++ nixos/_modules/v2ray-tailored/client/dns.nix
@@ -4,30 +4,25 @@
     "dns.alidns.com" = [ "223.6.6.6" "223.5.5.5" ];
     "dns.rubyfish.cn" = "212.129.148.151";
   } // extraHosts;
-  servers = [
-    {
+  servers = [{
       address = "100.100.100.100";  # MagicDNS of tailscale, setup tailscale with `tailscale up --accept-dns=false`
       domains = [ "domain:ts.net" ];
       skipFallback = true;
-    }
-    {
+    } {
       address = "10.10.0.21";
       port = 53;
       domains = [ "cc98" "nexushd" "domain:zju.edu.cn" ];
       skipFallback = true;
-    }
-    {
+    } {
       address = "https://dns.alidns.com/dns-query";
       domains = [ "domain:ntp.org" ];
       skipFallback = true;
-    }
-    {
+    } {
       address = "https://dns.alidns.com/dns-query";
       domains = [ "geosite:cn" ];
       expectIPs = [ "geoip:cn" ];
       skipFallback = true;
-    }
-    {
+    } {
       address = "https://dns.google/dns-query";
       domains = [ "geosite:geolocation-!cn" ];
       expectIPs = [ "geoip:!cn" ];
