4e5d19295b30bd78fa1f52b8145aa32e60cd2f06
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-13 15:42:45 +0800

    cargo: add registry credentials for publishing
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/secrets/default.nix
+++ home/gy/secrets/default.nix
@@ -5,6 +5,7 @@
     secrets = {  # use %r for $XDG_RUNTIME_DIR, use %% for a literal `%`
       "file/netrc".path = "${config.home.homeDirectory}/.netrc";  # REF: <https://docs.wandb.ai/guides/track/advanced/environment-variables>
       "file/telegram-send.conf".path = "${config.xdg.configHome}/telegram-send.conf";
+      "file/cargo-credentials".path = "${config.home.sessionVariables.CARGO_HOME}/credentials";
     };
   };
 }
