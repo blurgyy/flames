f5bbe08d1c69a743814fc75569c531ac2f15e035
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-16 18:20:20 +0800

    flake: only generate outputs for {x86_64,aarch64}-linux
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
   # specified in the `inputs` attribute above.
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }: let
     my = import ./packages;
-  in flake-utils.lib.eachDefaultSystem (system: let
+  in flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: let
     overlaysInUse = [
       self.overlays.default
     ];
