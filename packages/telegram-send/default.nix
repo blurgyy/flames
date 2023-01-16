b8b0ddb5518be0b1636a9d344df1890ca644fc54
Gaoyang Zhang <gy@blurgy.xyz>
2023-01-16 17:19:45 +0800

    telegram-send: fix import error with python-telegram-bot>=20.0
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/telegram-send/default.nix
+++ packages/telegram-send/default.nix
@@ -2,6 +2,14 @@
   inherit (source) pname version src;
 
   propagatedBuildInputs = [ appdirs colorama python-telegram-bot ];
+  
+  # REF: <https://github.com/rahiel/telegram-send/issues/115#issuecomment-1371790720>
+  postPatch = ''
+    substituteInPlace telegram_send/telegram_send.py \
+      --replace "from telegram.constants import MAX_MESSAGE_LENGTH" \
+                "from telegram.constants import MessageLimit" \
+      --replace "MAX_MESSAGE_LENGTH" "MAX_TEXT_LENGTH"
+  '';
 
   meta = {
     homepage = "https://github.com/rahiel/telegram-send";
