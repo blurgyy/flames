0ea7b18c9727e1badbfcb9a1ac3ba266562392a8
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-04 20:46:28 +0800

    v2ray-loyalsoldier: minor update
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/v2ray-loyalsoldier/default.nix
+++ packages/v2ray-loyalsoldier/default.nix
@@ -1,4 +1,4 @@
-{ v2ray , loyalsoldier-geodata }: v2ray.overrideAttrs (oldAttrs: {
+{ v2ray, loyalsoldier-geodata }: v2ray.overrideAttrs (oldAttrs: {
   pname = "v2ray-loyalsoldier";
   assetsDrv = loyalsoldier-geodata;
 })
