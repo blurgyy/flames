d8fdc96ba08bcc044e07c114e418da3673b848dd
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-08 15:08:21 +0800

    opi3lts-uboot: fix platforms
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/opi3lts-uboot/default.nix
+++ packages/opi3lts-uboot/default.nix
@@ -1,6 +1,6 @@
 { armTrustedFirmwareAllwinner, buildUBoot }: buildUBoot {
   defconfig = "orangepi_3_defconfig";
-  extraMeta.platforms = ["aarch64_linux"];
+  extraMeta.platforms = ["aarch64-linux"];
   BL31 = "${armTrustedFirmwareAllwinner}/bl31.bin";
   filesToInstall = ["u-boot-sunxi-with-spl.bin"];
 }
