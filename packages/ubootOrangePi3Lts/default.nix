50872cc996f295fa1bdf90bf49e5c11dccaee0ec
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-09 04:13:01 +0800

    ubootOrangePi3Lts: try to simplify from ce5089a
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/ubootOrangePi3Lts/default.nix
+++ packages/ubootOrangePi3Lts/default.nix
@@ -1,8 +1,7 @@
 { writeText, buildUBoot
 , armTrustedFirmwareAllwinnerH6
 }: buildUBoot {
-  defconfig = "orangepi_3_lts_defconfig";
-  extraPatches = [ ./add-orangepi-3-lts-dtb.patch ];
+  defconfig = "orangepi_3_defconfig";
   extraMeta.platforms = ["aarch64-linux"];
   BL31 = "${armTrustedFirmwareAllwinnerH6}/bl31.bin";
   filesToInstall = ["u-boot-sunxi-with-spl.bin"];
