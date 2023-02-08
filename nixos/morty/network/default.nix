2e59887b8204e03e1bfadd660153e3473d6ee7e3
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-09 05:19:33 +0800

    morty: fix closing hp lid soft blocks wifi
    REF: <https://itectec.com/ubuntu/ubuntu-does-airplane-mode-keep-toggling-on-the-hp-laptop-in-ubuntu-18-04/>
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/morty/network/default.nix
+++ nixos/morty/network/default.nix
@@ -1,7 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./rathole.nix
     ../../_parts/vclient.nix
     ./wlan.nix
   ];
+  systemd.services.hp-keycodes = {
+    wantedBy = [
+      "multi-user.target"
+      "rescue.target"
+      "graphical.target"
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      # REF: <https://itectec.com/ubuntu/ubuntu-does-airplane-mode-keep-toggling-on-the-hp-laptop-in-ubuntu-18-04/>
+      ExecStart = "${pkgs.kbd}/bin/setkeycodes e057 240 e058 240";
+    };
+  };
 }
