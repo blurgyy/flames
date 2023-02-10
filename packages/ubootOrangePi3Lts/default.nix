a31af88c588cb337506011b622d5edd8edef198d
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-10 20:52:03 +0800

    ubootOrangePi3Lts: lock u-boot version to v2020.04 and update patch
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/ubootOrangePi3Lts/default.nix
+++ packages/ubootOrangePi3Lts/default.nix
@@ -1,9 +1,19 @@
-{ writeText, buildUBoot
+{ writeText, buildUBoot, fetchFromGitHub
 , armTrustedFirmwareAllwinnerH6
-}: buildUBoot {
+}: (buildUBoot rec {
+  # NOTE: upstream uses o-boot v2020.04
+  # REF: <https://github.com/orangepi-xunlong/u-boot-orangepi/branches>
+  version = "2020.04";
+  src = fetchFromGitHub {
+    owner = "u-boot";
+    repo = "u-boot";
+    rev = "v${version}";
+    hash = "sha256-4FuqNROiSTVNavfmcJ+K67pJhCsAlPLYn2/aXjvV5Eo=";
+  };
   defconfig = "orangepi_3_lts_defconfig";
-  extraPatches = [ ./add-orangepi-3-lts-dtb.patch ];
   extraMeta.platforms = ["aarch64-linux"];
   BL31 = "${armTrustedFirmwareAllwinnerH6}/bl31.bin";
   filesToInstall = ["u-boot-sunxi-with-spl.bin"];
+}).override {
+  patches = [ ./add-support-for-orangepi-3-lts.patch ];
 }
