f6a5184545a64d9c6dd0267ff04df0597f271855
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-19 10:52:06 +0800

    sshga: forward extra socket from host machine to remote machine
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/sshga/default.nix
+++ packages/sshga/default.nix
@@ -1,5 +1,5 @@
 { writeShellScriptBin }: writeShellScriptBin "sshga" ''
-  host_gpg_agent_socket="$(gpgconf --list-dirs agent-socket)"
-  remote_gpg_agent_socket="$host_gpg_agent_socket"
+  host_gpg_agent_socket="$(gpgconf --list-dirs agent-extra-socket)"
+  remote_gpg_agent_socket="$(gpgconf --list-dirs agent-socket)"
   exec ssh -R "$remote_gpg_agent_socket:$host_gpg_agent_socket" "$@"
 ''
