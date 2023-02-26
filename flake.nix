6b06297b01e867109f1a5a7cdb8f3756a62c851a
Gaoyang Zhang <gy@blurgy.xyz>
2023-02-26 21:07:35 +0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'aliyundrive-mediaserver':
        'gitlab:highsunz/aliyundrive-mediaserver/42eea4241a851fc72d939c2b402cb2a55d346ef1' (2023-02-26)
      → 'gitlab:highsunz/aliyundrive-mediaserver/9d62129f2286ed8bb06b548cccdb03337cec53ca' (2023-02-26)
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    aliyundrive-mediaserver.url = "gitlab:highsunz/aliyundrive-mediaserver/42eea4241a851fc72d939c2b402cb2a55d346ef1";
+    aliyundrive-mediaserver.url = "gitlab:highsunz/aliyundrive-mediaserver/9d62129f2286ed8bb06b548cccdb03337cec53ca";
     orangepi-3-lts-nixos.url = "gitlab:highsunz/orangepi-3-lts-nixos";
     acremote.url = "gitlab:highsunz/acremote";
     nvfetcher.url = "github:berberman/nvfetcher";
