5c49aa1ca2acbbf74e31efefc13ea0943cdb8a18
Gaoyang Zhang <gy@blurgy.xyz>
2023-03-01 20:18:50 +0800

    home: replace all invalid characters on `cact`
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/raw/fish/functions/cact.fish
+++ home/gy/parts/raw/fish/functions/cact.fish
@@ -3,7 +3,8 @@ function __conda_envset
   if set -q fish_history
     set -gx old_fish_history "$fish_history"
   end
-  set -gx fish_history "condaenv_$(string replace --all -- - _ "$argv[1]")"
+  set -l invalid_chars '/|-|\.|@|#|%'
+  set -gx fish_history "condaenv_$(string replace --all --regex -- "$invalid_chars" _ "$argv[1]")"
   # remove from global function space
   functions --erase __conda_envset
 end
