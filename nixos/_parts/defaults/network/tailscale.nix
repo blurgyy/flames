fe2927d7c9edf7a573dd0171804338667f34d464
Gaoyang Zhang <gy@blurgy.xyz>
2023-03-04 00:50:15 +0800

    nixos: suppress overly verbose tailscaled logs
    REF:
    - <https://www.reddit.com/r/Tailscale/comments/vholvd/comment/j52g3no>
    - man:systemd.exec(5)
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/network/tailscale.nix
+++ nixos/_parts/defaults/network/tailscale.nix
@@ -3,6 +3,10 @@
 in {
   services.tailscale.enable = true;
   networking.resolvconf.extraConfig = lib.mkAfter "search_domains=${tailnet}";  # If managing DNS manually instead of using systemd-resolved, use `tailscale up --accept-dns=false`
+  # REF:
+  # - <https://www.reddit.com/r/Tailscale/comments/vholvd/comment/j52g3no>
+  # - man:systemd.exec(5)
+  systemd.services.tailscaled.serviceConfig.LogLevelMax = "notice";
   boot.kernel.sysctl = {  # use `tailscale up --advertise-exit-node`
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
