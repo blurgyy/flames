90dec8c9bbac5a4accf109fec70f09e32b51b6c5
Gaoyang Zhang <gy@blurgy.xyz>
2023-04-22 21:58:37 +0800

    copi: reboot machine on possible network failure
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/copi/network/wlan.nix
+++ nixos/copi/network/wlan.nix
@@ -24,56 +24,82 @@
     groups.zjuwlan = {};
   };
 
-  systemd = {
-    timers.zjuwlan-login = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "60s";
-        OnUnitInactiveSec = "180s";
-        Persistent = true;
-        RandomizedDelaySec = "120s";
-        Unit = "zjuwlan-login.service";
-      };
+  systemd.timers.zjuwlan-login = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "60s";
+      OnUnitInactiveSec = "180s";
+      Persistent = true;
+      RandomizedDelaySec = "120s";
+      Unit = "zjuwlan-login.service";
     };
-    services.zjuwlan-login = {
-      path = with pkgs; [
-        coreutils-full
-        curl
-        diffutils
-        firefox-unwrapped
-        gnugrep
-        iw
-      ];
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecCondition = let
-          zjuwlan-login-condition = pkgs.writeShellScriptBin "zjuwlan-login-condition" ''
-            function current_ssid() {
-              iw dev | grep ssid | cut -d' ' -f2
-            }
-            if ! cmp -s <(current_ssid | head -c3) <(echo -n ZJU); then
-              echo "not connected to ZJUWLAN, skipping"
-              exit 1
-            fi
-            if curl -fsSL https://www.baidu.com/ | grep -q "百度一下"; then
-              echo "already logged in, skipping"
-              exit 2
-            fi
-          '';
-        in "${zjuwlan-login-condition}/bin/zjuwlan-login-condition";
-        RuntimeMaxSec = 90;
-        Restart = "on-failure";
-        RestartSec = 5;
-        User = config.users.users.zjuwlan.name;
-        Group = config.users.groups.zjuwlan.name;
-        PrivateTmp = true;
-        LoadCredential = "credentials:${config.sops.secrets."zjuwlan-credentials".path}";
-      };
-      script = ''
-        cd "$CREDENTIALS_DIRECTORY"
-        ${pkgs.zjuwlan-login-script}/bin/zjuwlan
-      '';
+  };
+  systemd.services.zjuwlan-login = {
+    path = with pkgs; [
+      coreutils-full
+      curl
+      diffutils
+      firefox-unwrapped
+      gnugrep
+      iw
+    ];
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecCondition = let
+        zjuwlan-login-condition = pkgs.writeShellScriptBin "zjuwlan-login-condition" ''
+          function current_ssid() {
+            iw dev | grep ssid | cut -d' ' -f2
+          }
+          if ! cmp -s <(current_ssid | head -c3) <(echo -n ZJU); then
+            echo "not connected to ZJUWLAN, skipping"
+            exit 1
+          fi
+          if curl -fsSL https://www.baidu.com/ --connect-timeout 5 | grep -q "百度一下"; then
+            echo "already logged in, skipping"
+            exit 2
+          fi
+        '';
+      in "${zjuwlan-login-condition}/bin/zjuwlan-login-condition";
+      RuntimeMaxSec = 90;
+      Restart = "on-failure";
+      RestartSec = 5;
+      User = config.users.users.zjuwlan.name;
+      Group = config.users.groups.zjuwlan.name;
+      PrivateTmp = true;
+      LoadCredential = "credentials:${config.sops.secrets."zjuwlan-credentials".path}";
+    };
+  };
+
+  systemd.timers.reboot-on-network-failure = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "300s";
+      OnUnitInactiveSec = "300s";
+      Persistent = true;
+      RandomizedDelaySec = "60s";
+      Unit = "reboot-on-network-failure.service";
+    };
+  };
+  systemd.services.reboot-on-network-failure = {
+    path = with pkgs; [
+      curl
+      gnugrep
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.systemd}/bin/systemctl reboot";
+      ExecCondition = let
+        network-failure-condition = pkgs.writeShellScriptBin "network-failure-condition" ''
+          if curl -fsSL https://www.baidu.com/ --connect-timeout 10 | grep -q "百度一下"; then
+            echo "network is up and running, skipping"
+            exit 1
+          fi
+        '';
+      in "${network-failure-condition}/bin/network-failure-condition";
     };
   };
 }
