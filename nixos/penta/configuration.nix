a0962a59ee6994d84c3b315d2d69907a9472f903
Gaoyang Zhang <gy@blurgy.xyz>
2023-06-30 00:40:40 +0800

    penta: init machine
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ nixos/penta/configuration.nix
@@ -0,0 +1,33 @@
+{ ... }: {
+  time.timeZone = "Europe/Berlin";
+
+  boot.loader.grub.device = "/dev/sda";
+  fileSystems = {
+    "/" = {  
+      device = "/dev/disk/by-label/nixos-root";
+      fsType = "btrfs";
+      options = [ "noatime" "compress-force=zstd:3" "discard=async" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/nixos-boot";
+      fsType = "vfat";
+    };
+  };
+
+  systemd.network.wait-online.extraArgs = [ "--interface=eth0" ];
+
+  networking = {
+    defaultGateway = "109.71.253.1";
+    interfaces.eth0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "109.71.253.196";
+        prefixLength = 24;
+      }];
+    };
+  };
+
+  services.udisks2.enable = false;
+
+  system.stateVersion = "23.11";
+}
---
+++ nixos/penta/services/default.nix
@@ -0,0 +1,22 @@
+{ config, ... }: {
+  imports = [
+    ../../_parts/hydra-distributed-builder.nix
+    ./v2ray.nix
+  ];
+  sops.secrets.acme-credentials-file = with config.users; {
+    owner = users.haproxy.name;
+    group = groups.haproxy.name;
+  };
+  services.haproxy-tailored = {
+    enable = true;
+    frontends.tls-offload-front = {
+      domain = {
+        acme = {
+          enable = true;
+          email = "gy@blurgy.xyz";
+          credentialsFile = config.sops.secrets.acme-credentials-file.path;
+        };
+      };
+    };
+  };
+}
