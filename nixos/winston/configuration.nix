d7295ac14ca1565c881709ce9155ec5b59269203
Gaoyang Zhang <gy@blurgy.xyz>
2023-07-07 18:36:03 +0800

    winston: enable SSH compresson on sshfs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/winston/configuration.nix
+++ nixos/winston/configuration.nix
@@ -25,7 +25,7 @@
     "/nfs" = {
       device = "/run/current-system/sw/bin/sshfs#gy@localhost:/home/gy/.local/share/nfs";
       fsType = "fuse";
-      options = [ "idmap=user" "Port=13815,Compression=no,IdentityFile=/home/gy/.ssh/id_ed25519,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no" "_netdev" "allow_other" ];
+      options = [ "idmap=user" "Port=13815,Compression=yes,IdentityFile=/home/gy/.ssh/id_ed25519,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no" "_netdev" "allow_other" ];
     };
     "/elements" = {
       device = "/dev/disk/by-label/wd-elements";
