6f74b245dc928ae9533e9fb07f4a9aebccda2c2e
Gaoyang Zhang <gy@blurgy.xyz>
2023-07-18 16:30:36 +0800

    flake: make inputs follow nixpkgs-stable specified from this flake
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       url = "github:Mic92/sops-nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs-stable";
       };
     };
     nix-index-db = {
