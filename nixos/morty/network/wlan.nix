bbcd5f59ff28d0dc37be80ff31db3566d09e5027
Gaoyang Zhang <gy@blurgy.xyz>
2023-08-04 13:18:10 +0800

    nixis: update WLAN configs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/morty/network/wlan.nix
+++ nixos/morty/network/wlan.nix
@@ -6,11 +6,13 @@
     networks = {
       "@wlan_0@" = { psk = "@wlan_0_psk@"; priority = 100; };
       "@wlan_1@" = { psk = "@wlan_1_psk@"; priority = 100; };
-      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 75; };
-      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 50; };
-      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 30; };
-      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 30; };
+      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 100; };
+      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 75; };
+      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 75; };
+      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 50; };
       "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 30; };
+      "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 30; };
+      "@wlan_8@" = { psk = "@wlan_8_psk@"; priority = 30; };
       "ZJUWLAN".authProtocols = [ "NONE" ];
       "ZJUWLAN-NEW".authProtocols = [ "NONE" ];
     };
