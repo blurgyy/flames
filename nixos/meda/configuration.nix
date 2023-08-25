d84c06589d4924784ae8a2cbc2626342251c030a
Gaoyang Zhang <gy@blurgy.xyz>
2023-08-25 16:15:11 +0800

    meda: use stable nixpkgs for cuda needed by openai-whisper
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/meda/configuration.nix
+++ nixos/meda/configuration.nix
@@ -1,4 +1,12 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, inputs, ... }:
+
+let
+  pkgs-stable = import inputs.nixpkgs-stable {
+    inherit (pkgs) system config overlays;
+  };
+in
+
+{
   time.timeZone = "Asia/Shanghai";
 
   wsl = {
@@ -11,7 +19,7 @@
   environment = {
     systemPackages = [
       config.boot.kernelPackages.nvidiaPackages.stable  # provides the nvidia-smi binary
-      (pkgs.openai-whisper.override { cudaSupport = true; })
+      (pkgs-stable.openai-whisper.override { cudaSupport = true; })
     ];
     sessionVariables.LD_LIBRARY_PATH = [
       "/usr/lib/wsl/lib"  # required for nvidia-smi to work
