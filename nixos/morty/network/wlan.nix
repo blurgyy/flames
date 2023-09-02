6dcbd89a8bdf5b9e415f0249ca4b84f6c0bc0d8a
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-02 21:14:08 +0800

    morty: update WLAN configs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/morty/network/wlan.nix
+++ nixos/morty/network/wlan.nix
@@ -13,6 +13,7 @@
       "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 30; };
       "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 30; };
       "@wlan_8@" = { psk = "@wlan_8_psk@"; priority = 30; };
+      "@wlan_9@" = { psk = "@wlan_9_psk@"; priority = 30; };
       "ZJUWLAN".authProtocols = [ "NONE" ];
       "ZJUWLAN-NEW".authProtocols = [ "NONE" ];
       "HUAZHU-JiHotel".authProtocols = [ "NONE" ];
