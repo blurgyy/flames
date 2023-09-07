74a3561a2fceac6784029b0e7b27ded22ee2de88
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-07 21:51:57 +0800

    blender-ply-as-verts: do not use symlinks for adding the ply-as-verts script
    This ensures blender loads the overridden script instead of the original
    script shipped with blender.
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/blender-ply-as-verts/default.nix
+++ packages/blender-ply-as-verts/default.nix
@@ -2,18 +2,7 @@
 , blender
 }: with lib; blender.overrideAttrs (o: {
   pname = o.pname + "-import-ply-as-verts";
-  buildCommand = ''
-    set -euo pipefail
-    ${  # REF: <https://stackoverflow.com/a/68523368/13482274>
-      lib.concatStringsSep "\n" (map (outputName: ''
-        echo "Copying output ${outputName}"
-        set -x
-        cp -rs --no-preserve=mode "${blender.${outputName}}" "''$${outputName}"
-        set +x
-      '')
-      (o.outputs or ["out"]))
-    }
-
+  postInstall = o.postInstall or "" + ''
     install -Dvm444 -t \
       $out/share/blender/${concatStringsSep "." (take 2 (splitVersion o.version))}/scripts/addons/io_mesh_ply \
       ${generated.import-ply-as-verts-for-blender.src}/Blender_Files/{__init__,import_ply}.py
