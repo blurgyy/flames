1c9c058a72f9f3891cc2af7a827c9ace248472ca
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-13 13:01:53 +0800

    printer-cadlab: look for files in XDG_RUNTIME_DIR and fallback to /tmp
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/printer-cadlab/default.nix
+++ packages/printer-cadlab/default.nix
@@ -2,5 +2,5 @@
 , coreutils-full
 , rdesktop
 }: writeShellScriptBin "printer" ''
-  ${coreutils-full}/bin/yes 'yes' | ${rdesktop}/bin/rdesktop 10.76.2.85 -u cadliu -p lxg -r disk:h=/home/gy/Playground/printer
+  ${coreutils-full}/bin/yes 'yes' | ${rdesktop}/bin/rdesktop 10.76.2.85 -u cadliu -p lxg -r disk:h=''${XDG_RUNTIME_DIR:-/tmp}/printer
 ''
