48e1fb767009255c340f042ceeab987d87fbabd3
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-17 11:36:45 +0800

    blender-ply-as-verts{,-cuda}: move all overrides to derivation `blender-ply-as-verts`
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/blender-ply-as-verts-cuda/default.nix
+++ packages/blender-ply-as-verts-cuda/default.nix
@@ -1,17 +1,3 @@
-{ generated, lib
-, blender
-}: with lib; (blender.override { cudaSupport = true; }).overrideAttrs (o: {
-  pname = o.pname + "-import-ply-as-verts-cuda";
+{ blender-ply-as-verts }:
 
-  # nvcc from cudatoolkit at nixpkgs/23.05 does not support sm_89
-  postPatch = ''
-    sed -Ee 's/ sm_89 / /g' -i CMakeLists.txt
-  '';
-
-  postInstall = o.postInstall or "" + ''
-    install -Dvm444 -t \
-      $out/share/blender/${concatStringsSep "." (take 2 (splitVersion o.version))}/scripts/addons/io_mesh_ply \
-      ${generated.import-ply-as-verts-for-blender.src}/Blender_Files/{__init__,import_ply}.py
-  '';
-  meta.platforms = [ "x86_64-linux" ];
-})
+blender-ply-as-verts.override { cudaSupport = true; }
--- packages/blender-ply-as-verts/default.nix
+++ packages/blender-ply-as-verts/default.nix
@@ -1,7 +1,12 @@
-{ generated, lib
+{ config, generated, lib
 , blender
-}: with lib; blender.overrideAttrs (o: {
-  pname = o.pname + "-import-ply-as-verts";
+, cudaSupport ? config.cudaSupport
+}:
+
+with lib;
+
+(blender.override { inherit cudaSupport; }).overrideAttrs (o: {
+  pname = o.pname + "-import-ply-as-verts" + (if cudaSupport then "-cuda" else "");
 
   # nvcc from cudatoolkit at nixpkgs/23.05 does not support sm_89
   postPatch = ''
@@ -13,4 +18,6 @@
       $out/share/blender/${concatStringsSep "." (take 2 (splitVersion o.version))}/scripts/addons/io_mesh_ply \
       ${generated.import-ply-as-verts-for-blender.src}/Blender_Files/{__init__,import_ply}.py
   '';
+
+  meta.platforms = [ "x86_64-linux" ];
 })
