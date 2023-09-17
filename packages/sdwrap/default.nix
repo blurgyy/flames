b2c18f95226decfcaeab9c061b8bd13d56ffabec
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-18 07:57:08 +0800

    sdwrap: add completion in fish shell
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/sdwrap/default.nix
+++ packages/sdwrap/default.nix
@@ -1,7 +1,14 @@
-{ writeShellScriptBin, symlinkJoin }: let
+{ writeShellScriptBin, stdenvNoCC, symlinkJoin }: let
   sdwrap = writeShellScriptBin "sdwrap" (builtins.readFile ./src/sdwrap);
   sdwrap-clean = writeShellScriptBin "sdwrap-clean" (builtins.readFile ./src/sdwrap-clean);
+  sdwrap-fish-completions = stdenvNoCC.mkDerivation {
+    name = "sdwrap-fish-completions";
+    src = ./src/fish-completions;
+    buildCommand = ''
+      install -Dvm555 -t $out/share/fish/completions $src/*
+    '';
+  };
 in symlinkJoin {
   name = "sdwrap";
-  paths = [ sdwrap sdwrap-clean ];
+  paths = [ sdwrap sdwrap-clean sdwrap-fish-completions ];
 }
---
+++ packages/sdwrap/src/fish-completions/sdwrap-clean.fish
@@ -0,0 +1,2 @@
+# REF: /share/fish/completions/exec.fish
+complete -c sdwrap-clean -xa "(__fish_complete_subcommand)"
