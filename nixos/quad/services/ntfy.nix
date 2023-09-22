080d0d2079e024968d7b9364dd7af152e6afa074
Gaoyang Zhang <gy@blurgy.xyz>
2023-09-22 19:56:39 +0800

    quad: add missing default haproxy backend
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/quad/services/ntfy.nix
+++ nixos/quad/services/ntfy.nix
@@ -1,6 +1,17 @@
-{ ... }: {
-  services.ntfy-tailored = {
-    enable = true;
-    domain = "ntfy.blurgy.xyz";
+{ config, ... }: {
+  services = {
+    ntfy-tailored = {
+      enable = true;
+      domain = "ntfy.blurgy.xyz";
+    };
+    haproxy-tailored = {
+      frontends.tls-offload-front.backends = [
+        { name = "ntfy-default"; isDefault = true; }
+      ];
+      backends.ntfy-default = {
+        mode = "http";
+        server.address = "127.0.0.1:${toString config.services.ntfy-tailored.port}";
+      };
+    };
   };
 }
