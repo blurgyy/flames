2605980ada13d024c723efbd6d0d8dcbb08e4745
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-09 18:48:22 +0800

    loyalsoldier-geodata: add `hydra-build-products` file
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/loyalsoldier-geodata/default.nix
+++ packages/loyalsoldier-geodata/default.nix
@@ -1,7 +1,21 @@
 { symlinkJoin
 , loyalsoldier-geoip
 , loyalsoldier-geosite
-}: symlinkJoin {
-  name = "loyalsoldier-geodata";
-  paths = [ loyalsoldier-geoip loyalsoldier-geosite ];
-}
+}:
+
+let
+  joined = symlinkJoin {
+    name = "loyalsoldier-geodata";
+    paths = [ loyalsoldier-geoip loyalsoldier-geosite ];
+  };
+in
+
+joined.overrideAttrs (o: {
+  buildCommand = o.buildCommand + ''
+    mkdir $out/nix-support -p
+    cat >$out/nix-support/hydra-build-products <<EOF
+    file binary-dist $out/share/v2ray/geoip.dat
+    file binary-dist $out/share/v2ray/geosite.dat
+    EOF
+  '';
+})
