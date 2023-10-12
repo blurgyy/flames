23d2097c716e0a04024b26709b9256233f3fef47
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-13 02:42:13 +0800

    sing-box-tailored: add zju IP CIDR rules for proxying
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/route/rules/51-conditinal-zju.nix
+++ nixos/_modules/sing-box-tailored/route/rules/51-conditinal-zju.nix
@@ -8,5 +8,9 @@
   domain_suffix = [
     ".zju.edu.cn"
   ];
+  ip_cidr = [  # calculated with <https://www.ipaddressguide.com/cidr>
+    "10.189.128.0/7"  # ZJUWLAN
+    "10.76.0.0/21"    # CAD cable
+  ];
   outbound = zjuConditionalOutbound;
 }
