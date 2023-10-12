5231d415290688bd91c49a80d02bd35deccc7a02
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-13 00:28:01 +0800

    nixos: separate secret declarations from proxy configs
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ nixos/_parts/proxy-client-secrets.nix
@@ -0,0 +1,21 @@
+{ ... }: {
+  sops.secrets = {
+    "v2ray/observatory-probe-url" = {};
+    "v2ray/ports/client/http" = {};
+    "v2ray/ports/client/socks" = {};
+    "v2ray/ports/client/tproxy" = {};
+    "v2ray/ws-path" = {};
+    "v2ray/id" = {};
+    "v2ray/domains/eu-00" = {};
+    "v2ray/domains/eu-01" = {};
+    "v2ray/domains/jp-00" = {};
+    "v2ray/domains/us-00" = {};
+    "v2ray/domains/wss-eu-00" = {};
+    "v2ray/domains/wss-eu-01" = {};
+    "v2ray/addresses/cn-00" = {};
+    "v2ray/addresses/eu-00" = {};
+    "v2ray/addresses/eu-01" = {};
+    "v2ray/addresses/jp-00" = {};
+    "v2ray/addresses/us-00" = {};
+  };
+}
--- nixos/_parts/vclient.nix
+++ nixos/_parts/vclient.nix
@@ -1,23 +1,7 @@
 { config, ... }: {
-  sops.secrets = {
-    "v2ray/observatory-probe-url" = {};
-    "v2ray/ports/client/http" = {};
-    "v2ray/ports/client/socks" = {};
-    "v2ray/ports/client/tproxy" = {};
-    "v2ray/ws-path" = {};
-    "v2ray/id" = {};
-    "v2ray/domains/eu-00" = {};
-    "v2ray/domains/eu-01" = {};
-    "v2ray/domains/jp-00" = {};
-    "v2ray/domains/us-00" = {};
-    "v2ray/domains/wss-eu-00" = {};
-    "v2ray/domains/wss-eu-01" = {};
-    "v2ray/addresses/cn-00" = {};
-    "v2ray/addresses/eu-00" = {};
-    "v2ray/addresses/eu-01" = {};
-    "v2ray/addresses/jp-00" = {};
-    "v2ray/addresses/us-00" = {};
-  };
+  imports = [
+    ./proxy-client-secrets.nix
+  ];
   services.v2ray-tailored.client = {
     logging.level = "warning";
     enable = true;
