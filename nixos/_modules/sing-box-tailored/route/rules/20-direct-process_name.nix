0e90252ac763047da8f909b39a0dfc87b8113b1b
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-13 16:10:19 +0800

    network: update list of process names for direct connection
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/route/rules/20-direct-process_name.nix
+++ nixos/_modules/sing-box-tailored/route/rules/20-direct-process_name.nix
@@ -1,8 +1,16 @@
 {
   process_name = [
+    "nc"
+
+    "nscd"
+    "nsncd"
+
+    "sshd"
+
+    "systemd-timesyncd"
+
     "v2ray"
     ".v2ray-wrapped"
-    "sshd"
   ];
   outbound = "direct-process";
 }
