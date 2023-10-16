930f3a04696df0ee2376f8cd36fddc869d161a47
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-16 17:25:23 +0800

    cindy&penta: fix haproxy config for wss inbounds
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/cindy/services/v2ray.nix
+++ nixos/cindy/services/v2ray.nix
@@ -3,10 +3,19 @@
     ../../_parts/vserver.nix
   ];
   services.haproxy-tailored = {
-    frontends.tls-offload-front.backends = [{ name = "pivot"; condition = "unless HTTP"; }];
-    backends.pivot = {
-      mode = "tcp";
-      server.address = "127.0.0.1:${config.services.v2ray-tailored.server.ports.tcp}";
+    frontends.tls-offload-front.backends = [
+      { name = "pivot"; condition = "unless HTTP"; }
+      { name = "pivot-wss"; condition = "if { path ${config.sops.placeholder."v2ray/ws-path"} }"; }
+    ];
+    backends = {
+      pivot = {
+        mode = "tcp";
+        server.address = "127.0.0.1:${config.services.v2ray-tailored.server.ports.tcp}";
+      };
+      pivot-wss = {
+        mode = "tcp";
+        server.address = "127.0.0.1:${config.services.v2ray-tailored.server.ports.wss}";
+      };
     };
   };
 }
