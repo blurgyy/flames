b6d4a7bd73c6a88f3852858b73f6d5dd0c24937a
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-21 15:42:33 +0800

    fix(sing-box-tailored): tun interface is not required for online
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/default.nix
+++ nixos/_modules/sing-box-tailored/default.nix
@@ -36,6 +36,7 @@ in
       '';
       systemd.network.networks."50-sing-box" = {  # Configs adapted from /etc/systemd/network/50-tailscale.network
         matchConfig.Name = cfg.tunInterface;
+        linkConfig.RequiredForOnline = false;
         linkConfig = {  
           ActivationPolicy = "manual";
           Unmanaged = true;
