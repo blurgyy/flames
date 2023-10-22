4052921d16e2ab6c029467ad0ee614ffe626ed1b
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-22 16:56:16 +0800

    sing-box-tailored: use direct IP url test to fix loop dependency while determining best internal net server
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/outbounds/urltest/auto-zju.nix
+++ nixos/_modules/sing-box-tailored/outbounds/urltest/auto-zju.nix
@@ -4,5 +4,8 @@
     "cn-00"
     "direct-zju-internal"
   ];
-  url = "http://www.cc98.org";
+  # WARN: Must use direct IP connection here, because both zju DNS also depends on this
+  url = "http://10.10.98.98/";  # www.cc98.org
+  # url = "http://10.50.200.3/";  # net2.zju.edu.cn
+  # url = "http://10.50.200.245/";  # net.zju.edu.cn
 }
