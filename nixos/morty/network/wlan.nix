667e43dc63ab20f460b10ef37fdfa9a477e9215a
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-24 10:39:31 +0800

    nixos(wlan): update wireless credentials
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/morty/network/wlan.nix
+++ nixos/morty/network/wlan.nix
@@ -4,17 +4,14 @@
     enable = true;
     environmentFile = config.sops.secrets.wireless-environment-file.path;
     networks = {
-      "@wlan_0@" = { psk = "@wlan_0_psk@"; priority = 100; };
-      "@wlan_1@" = { psk = "@wlan_1_psk@"; priority = 100; };
-      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 100; };
-      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 75; };
-      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 75; };
-      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 50; };
-      "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 30; };
-      "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 30; };
-      "@wlan_8@" = { psk = "@wlan_8_psk@"; priority = 30; };
-      "@wlan_9@" = { psk = "@wlan_9_psk@"; priority = 30; };
-      "HUAZHU-JiHotel".authProtocols = [ "NONE" ];
+      "@wlan_0@" = { psk = "@wlan_0_psk@"; priority = 90; };
+      "@wlan_1@" = { psk = "@wlan_1_psk@"; priority = 90; };
+      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 90; };
+      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 65; };
+      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 65; };
+      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 40; };
+      "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 20; };
+      "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 20; };
     };
   };
 }
--- nixos/opi/network/wlan.nix
+++ nixos/opi/network/wlan.nix
@@ -4,14 +4,23 @@
     enable = true;
     environmentFile = config.sops.secrets.wireless-environment-file.path;
     networks = {
-      "@wlan_0@" = { psk = "@wlan_0_psk@"; priority = 100; };
-      "@wlan_1@" = { psk = "@wlan_1_psk@"; priority = 100; };
-      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 100; };
-      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 75; };
-      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 75; };
-      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 50; };
-      "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 30; };
-      "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 30; };
+      "morty.ap" = {
+        psk = "@morty_ap_psk@";
+        priority = 100;
+        # REF: <https://wiki.archlinux.org/title/wpa_supplicant#Connections_to_mixed_WPA2-PSK/WPA3-SAE_access_points>
+        authProtocols = [ "WPA-PSK-SHA256" ];
+        extraConfig = ''
+          ieee80211w=2
+        '';
+      };
+      "@wlan_0@" = { psk = "@wlan_0_psk@"; priority = 90; };
+      "@wlan_1@" = { psk = "@wlan_1_psk@"; priority = 90; };
+      "@wlan_2@" = { psk = "@wlan_2_psk@"; priority = 90; };
+      "@wlan_3@" = { psk = "@wlan_3_psk@"; priority = 65; };
+      "@wlan_4@" = { psk = "@wlan_4_psk@"; priority = 65; };
+      "@wlan_5@" = { psk = "@wlan_5_psk@"; priority = 40; };
+      "@wlan_6@" = { psk = "@wlan_6_psk@"; priority = 20; };
+      "@wlan_7@" = { psk = "@wlan_7_psk@"; priority = 20; };
     };
   };
 
