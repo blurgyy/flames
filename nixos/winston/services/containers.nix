33cb76dcacbe2d623d451d336fb5d0d48cf6ab84
Gaoyang Zhang <gy@blurgy.xyz>
2023-10-26 12:08:04 +0800

    winston: use standard IP notation for https?_proxy variables for nix-daemon
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/winston/services/containers.nix
+++ nixos/winston/services/containers.nix
@@ -34,7 +34,7 @@
       '';
 
       proxy-env = builtins.toFile "nspawn-proxy-env" ''
-        export http_proxy=http://127.1:${toString config.services.ssh-reverse-proxy.server.services.http-proxy-from-copi.port}
+        export http_proxy=http://127.0.0.1:${toString config.services.ssh-reverse-proxy.server.services.http-proxy-from-copi.port}
         export https_proxy="$http_proxy"
       '';
       display-env = builtins.toFile "nspawn-display-env" ''
