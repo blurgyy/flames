8d53cae51dfb41ef5afe19c5d57b29f9b179bec3
Gaoyang Zhang <gy@blurgy.xyz>
2023-11-20 00:24:48 +0800

    blender-ply-as-verts{,-cuda}: fix build for import-ply-as-verts v3
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/blender-ply-as-verts/default.nix
+++ packages/blender-ply-as-verts/default.nix
@@ -1,5 +1,6 @@
 { config, generated, lib
 , blender
+, unzip
 , cudaSupport ? config.cudaSupport
 }:
 
@@ -8,15 +9,20 @@ with lib;
 (blender.override { inherit cudaSupport; }).overrideAttrs (o: {
   pname = o.pname + "-import-ply-as-verts" + (if cudaSupport then "-cuda" else "");
 
+  buildInputs = o.buildInputs or [] ++ [ unzip ];
+
   # nvcc from cudatoolkit at nixpkgs/23.05 does not support sm_89
-  postPatch = ''
+  postPatch = o.postPatch or "" + ''
     sed -Ee 's/ sm_89 / /g' -i CMakeLists.txt
   '';
 
   postInstall = o.postInstall or "" + ''
+    unzip ${generated.import-ply-as-verts-for-blender.src}/Import_PLY_as_Verts_3.zip
+    unzip PLY_As_Verts.zip
+
     install -Dvm444 -t \
       $out/share/blender/${concatStringsSep "." (take 2 (splitVersion o.version))}/scripts/addons/io_mesh_ply \
-      ${generated.import-ply-as-verts-for-blender.src}/Blender_Files/{__init__,import_ply}.py
+      Addon-Build/{__init__,{ex,im}port_ply}.py
   '';
 
   meta.platforms = [ "x86_64-linux" ];
