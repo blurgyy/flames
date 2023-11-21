ad50c58772e55ff028214cacdef68416f1eaacea
Gaoyang Zhang <gy@blurgy.xyz>
2023-11-21 23:13:53 +0800

    modules(v2ray-tailored): update
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/v2ray-tailored/server/routing/rules/10-direct-custom.nix
+++ nixos/_modules/v2ray-tailored/server/routing/rules/10-direct-custom.nix
@@ -5,6 +5,7 @@
     "domain:googleapis.com"
     "domain:cdn.jsdelivr.net"
     "domain:docs.microsoft.com"
+    "domain:statsig.com"  # openai auth for the ChatGPT iOS app requires <api.statsig.com> to be reachable
   ];
   outboundTag = "direct-custom";
 }
