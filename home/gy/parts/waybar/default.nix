b3553a3aa598de5cba01714fef061c897d8d7d05
Gaoyang Zhang <gy@blurgy.xyz>
2023-11-24 08:56:29 +0800

    waybar-hyprland: remove
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/waybar/default.nix
+++ home/gy/parts/waybar/default.nix
@@ -1,6 +1,5 @@
 { config, pkgs, callWithHelpers }: {
   enable = true;
-  package = pkgs.waybar-hyprland;
   settings = callWithHelpers ./settings.nix {};
   style = callWithHelpers ./style.css.nix {};
   systemd = {
--- packages/waybar-hyprland/default.nix
+++
@@ -1,9 +0,0 @@
-{ waybar }: waybar.overrideAttrs (o: {
-  pname = o.pname + "-hyprland";
-  postPatch = ''
-    # REF: <https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#waybar>
-    # NOTE: Remember to add hyprland's `bin` to waybar's runtime PATH
-    sed -Ee 's|zext_workspace_handle_v1_activate\(workspace_handle_\);|const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());|g' -i src/modules/wlr/workspace_manager.cpp
-  '';
-  mesonFlags = o.mesonFlags ++ [ "-Dexperimental=true" ];
-})
