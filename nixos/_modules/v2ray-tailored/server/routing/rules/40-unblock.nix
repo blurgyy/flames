dfa6cc543bf8d98c341d2289bcc25d7b66595771
Gaoyang Zhang <gy@blurgy.xyz>
2024-01-04 14:57:31 +0800

    module(v2ray-tailored): connect cloudflare domains/IPs through WARP
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/v2ray-tailored/server/routing/rules/40-unblock.nix
+++ nixos/_modules/v2ray-tailored/server/routing/rules/40-unblock.nix
@@ -2,6 +2,13 @@
   domain = [
     "domain:reddit.com"
     "domain:redd.it"
+    "domain:cloudflare.com"
+    "domain:cloudflare.net"
+    "domain:cloudflare-dns.com"
+  ];
+  ip_cidr = [
+    "1.1.1.1"
+    "1.0.0.1"
   ];
   outboundTag = "warp:unblock";
 }
