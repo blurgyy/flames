70ffce443796f4a98626de58d29730538d8fd98e
Gaoyang Zhang <gy@blurgy.xyz>
2024-01-16 10:22:40 +0800

    module(sing-box-tailored): use custom geosite rule sets
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/route/rule_set/geosite-cn.nix
+++ nixos/_modules/sing-box-tailored/route/rule_set/geosite-cn.nix
@@ -1,6 +1,6 @@
 {
   type = "remote";
   format = "binary";
-  url = "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-cn.srs";
+  url = "https://raw.githubusercontent.com/benetjohn/sing-geosite/rule-set/geosite-cn.srs";
   download_detour = "auto-oversea";
 }
