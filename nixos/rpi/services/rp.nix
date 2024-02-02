cb1820d328c79ad6908211492102d6dfba0595c6
Gaoyang Zhang <gy@blurgy.xyz>
2024-02-02 11:53:45 +0800

    rpi: enable zju proxy previously applied on copi
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/rpi/services/rp.nix
+++ nixos/rpi/services/rp.nix
@@ -1,9 +1,31 @@
-{ config, ... }: {
+{ config, lib, ... }:
+
+let
+  proxy-zju-host-port = 8952;
+in
+
+{
   sops.secrets = {
     "sshrp/ssh-env" = {};
     "sshrp/acremote-env" = {};
     "sshrp/http-proxy-env" = {};
     "sshrp/socks-proxy-env" = {};
+
+    "sshrp/proxy-zju-env" = {};
+
+    "v2ray/users/00/uuid" = {};
+    "v2ray/users/01/uuid" = {};
+    "v2ray/users/02/uuid" = {};
+    "v2ray/users/03/uuid" = {};
+    "v2ray/users/04/uuid" = {};
+    "v2ray/users/05/uuid" = {};
+
+    "v2ray/users/00/email" = {};
+    "v2ray/users/01/email" = {};
+    "v2ray/users/02/email" = {};
+    "v2ray/users/03/email" = {};
+    "v2ray/users/04/email" = {};
+    "v2ray/users/05/email" = {};
   };
 
   services.ssh-reverse-proxy = {
@@ -30,6 +52,39 @@
         environmentFile = config.sops.secrets."sshrp/socks-proxy-env".path;
         identityFile = config.sops.secrets.hostKey.path;
       };
+      proxy-zju-env = {
+        environmentFile = config.sops.secrets."sshrp/proxy-zju-env".path;
+        identityFile = config.sops.secrets.hostKey.path;
+        bindPort = 6096;
+        hostPort = proxy-zju-host-port;
+      };
     };
+  };
+
+  services.sing-box.settings = let
+    inboundTag = "zju-proxy-in";
+  in {
+    inbounds = [{
+      tag = inboundTag;
+
+      type = "vmess";
+      listen = "127.0.0.1";
+      listen_port = proxy-zju-host-port;
+      users = map
+        (index: {
+          name._secret = config.sops.secrets."v2ray/users/${index}/email".path;
+          uuid._secret = config.sops.secrets."v2ray/users/${index}/uuid".path;
+        })
+        [ "00" "01" "02" "03" "04" "05" ];
+    }];
+    route.rules = lib.mkBefore [{  # user mkBefore to prioritize this direct rule
+      inbound = inboundTag;
+      outbound = "direct-zju-internal";
+    } {
+      domain_suffix = "@custom/25-zju-domain-suffix@";
+      domain_keyword = "@custom/25-zju-domain-keyword@";
+      ip_cidr = "@custom/25-zju-ip@";
+      outbound = "direct-zju-internal";
+    }];
   };
 }
