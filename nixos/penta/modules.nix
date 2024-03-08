07c0439f273c7b79319f385d234019f441cb3d7c
Gaoyang Zhang <gy@blurgy.xyz>
2024-03-08 23:26:55 +0800

    penta: use latest hydra
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/penta/modules.nix
+++ nixos/penta/modules.nix
@@ -3,15 +3,8 @@
   ./network
   ./services
   {
-    nixpkgs.overlays = [(final: prev: let
-      pkgs-stable = import inputs.nixpkgs-stable {
-        inherit (prev) system config;
-      };
-    in {
+    nixpkgs.overlays = [(final: prev: {
       carinae = inputs.carinae.packages.${system}.default;
-      hydra-unstable = pkgs-stable.hydra-unstable.override {
-        nix = pkgs-stable.nixStable;
-      };
     })];
   }
 ]
