d95b34763aa83d0794f403ca5a0a0f5a029ab231
Gaoyang Zhang <gy@blurgy.xyz>
2024-03-23 22:02:04 +0800

    home(gpg-agent): use tty pinentry package again
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/gy/parts/gpg-agent.nix
+++ home/gy/parts/gpg-agent.nix
@@ -3,7 +3,7 @@
     gpg-agent = {
       enable = true;
       enableExtraSocket = true;
-      pinentryPackage = pkgs.pinentry-rofi;
+      pinentryPackage = pkgs.pinentry-tty;
       defaultCacheTtl = 3600 * 24;
       maxCacheTtl = 3600 * 48;
       enableScDaemon = false;
