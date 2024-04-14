654581f953ce9a64efbcaa0f04474085bf07be58
Gaoyang Zhang <gy@blurgy.xyz>
2024-04-14 20:40:42 +0800

    home: more robust quit as last buffer for nvim-tree
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/_parts/neovim/config.lua.nix
+++ home/_parts/neovim/config.lua.nix
@@ -898,7 +898,22 @@ in ''
   end
   vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
   ---- REF: https://github.com/kyazdani42/nvim-tree.lua#tips--reminders
-  vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+  vim.api.nvim_create_autocmd("QuitPre", {
+    callback = function()
+      local invalid_win = {}
+      local wins = vim.api.nvim_list_wins()
+      for _, w in ipairs(wins) do
+        local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+        if bufname:match("NvimTree_") ~= nil then
+          table.insert(invalid_win, w)
+        end
+      end
+      if #invalid_win == #wins - 1 then
+        -- Should quit, so we close all invalid windows.
+        for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+      end
+    end
+  })
 
   --- gitsigns
   require("gitsigns").setup({
