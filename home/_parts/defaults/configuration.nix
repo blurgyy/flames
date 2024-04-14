77bb0678fbe15e27591c64a5a76af274c7baf4b7
Gaoyang Zhang <gy@blurgy.xyz>
2024-04-14 14:28:26 +0800

    Revert "home: do not autostart tmux inside vscode's terminal"
    This reverts commit 2a8bc417f9543407131a7770a2c8de7b8591d5fb.
    
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/_parts/defaults/configuration.nix
+++ home/_parts/defaults/configuration.nix
@@ -111,13 +111,11 @@ in {
       initExtra = ''
         if [[ "$-" == *i*
            && -z "''${noexecfish+1}"
-           && -e "/run/host/container-manager"
-           && -n "$VSCODE_INJECTION" ]]; then
+           && -e "/run/host/container-manager" ]]; then
           echo "Executing fish from bash because at least one of the following is true:"
           echo "  1) this is an interactive shell"
           echo "  2) the environment variable 'noexecfish' is not set"
           echo "  3) we are inside a container"
-          echo "  4) we are inside a shell started by vscode"
           exec fish
         fi
       '';
