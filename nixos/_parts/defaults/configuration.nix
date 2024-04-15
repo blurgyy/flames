d061954e5504dd1db1b762823476077a592df00d
Gaoyang Zhang <gy@blurgy.xyz>
2024-04-16 07:55:58 +0800

    nixos: remove substituter "cache.blurgy.xyz"
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/configuration.nix
+++ nixos/_parts/defaults/configuration.nix
@@ -49,14 +49,12 @@ in {
         "https://cache.garnix.io"
         "https://nix-community.cachix.org"
         "https://cuda-maintainers.cachix.org"
-        "https://cache.blurgy.xyz"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "cache.blurgy.xyz:Xg9PvXkUIAhDIsdn/NOUUFo+HHc8htSiGj7O6fUj/W4="
       ];
       auto-optimise-store = true;
       # REF: <https://docs.cachix.org/faq#why-is-nix-not-picking-up-on-any-of-the-pre-built-artifacts>
