b25e4242d9b71b9d7766564a8ce68ef983159d46
Gaoyang Zhang <gy@blurgy.xyz>
2024-05-19 12:46:01 +0800

    home: fix conda binary searching logic
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/_parts/raw/fish/functions/__find_conda_bin.fish
+++ home/_parts/raw/fish/functions/__find_conda_bin.fish
@@ -2,21 +2,21 @@
 
 if set -q conda_search_path
   # respect existing environment, create a file under `~/.config/fish/conf.d/` to set it
-  set --local --append conda_search_path $HOME/.conda
+  set --function --append conda_search_path $HOME/.conda
 else
-  set --local conda_search_path $HOME/.conda
+  set --function conda_search_path $HOME/.conda
 end
 
-set --local --append conda_search_path $HOME/.local/lib/miniconda3
-set --local --append conda_search_path $HOME/.local/lib/miniconda
-set --local --append conda_search_path $HOME/miniconda3
-set --local --append conda_search_path $HOME/miniconda
-set --local --append conda_search_path $HOME/.local/lib/anaconda3
-set --local --append conda_search_path $HOME/.local/lib/anaconda
-set --local --append conda_search_path $HOME/anaconda3
-set --local --append conda_search_path $HOME/anaconda
-set --local --append conda_search_path /opt/miniconda
-set --local --append conda_search_path /opt/anaconda
+set --function --append conda_search_path $HOME/.local/lib/miniconda3
+set --function --append conda_search_path $HOME/.local/lib/miniconda
+set --function --append conda_search_path $HOME/miniconda3
+set --function --append conda_search_path $HOME/miniconda
+set --function --append conda_search_path $HOME/.local/lib/anaconda3
+set --function --append conda_search_path $HOME/.local/lib/anaconda
+set --function --append conda_search_path $HOME/anaconda3
+set --function --append conda_search_path $HOME/anaconda
+set --function --append conda_search_path /opt/miniconda
+set --function --append conda_search_path /opt/anaconda
 for csp in $conda_search_path
   if test -x "$csp/condabin/conda"
     echo "$csp/condabin/conda"
