075b9870413b3efb4092493d50297505830dea87
Gaoyang Zhang <gy@blurgy.xyz>
2024-06-05 14:03:08 +0800

    tdesktop-lily: fix build
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/tdesktop-lily/default.nix
+++ packages/tdesktop-lily/default.nix
@@ -3,18 +3,23 @@
 }:
 
 tdesktop.overrideAttrs (o: {
-  pname = "tdesktop-megumifox";
+  pname = "tdesktop-lily";
   inherit (source) version src;
-  patchFlags = "-lNp1";  # See the `prepare()` function in the PKGBUILD from archlinuxcn: <https://github.com/archlinuxcn/repo/tree/master/archlinuxcn/telegram-desktop-megumifox/PKGBUILD>
   cmakeFlags = o.cmakeFlags ++ [
     "-DTDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME=ON"
   ];
   buildInputs = o.buildInputs ++ [
     qt6.qt5compat
   ];
+  # See the `prepare()` function in the PKGBUILD from archlinuxcn: <https://github.com/archlinuxcn/repo/blob/1df7cb1204df5c548b8250f4a67af28f511c5897/archlinuxcn/telegram-desktop-lily/PKGBUILD#L44>
   postPatch = ''
-    patch --verbose -b -d Telegram/lib_ui/ -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-megumifox/0001-Use-font-from-environment-variables.patch
-    patch --verbose -b -lNp1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-megumifox/0002-add-TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME-back.patch
+    patch -b -l -d Telegram/lib_ui -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/no-embolded-font.patch
+    patch -b -l -d Telegram/lib_ui -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/dont-adjust-fontsize.patch
+    patch -b -l -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/0002-add-TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME-back.patch
+    patch -b -l -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/0003-make-blockquote-markup-less-distractive.patch
+    patch -b -l -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/0004-Drop-usage-of-Qt-6.5-colorScheme-API-on-Linux.patch
+    patch -b -l -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/animated-avatar-follows-power-setting.patch
+    patch -b -l -Np1 -i ${generated.alcn-repo.src}/archlinuxcn/telegram-desktop-lily/warn-before-delete-all-2.patch
   '';
   meta.platforms = [ "x86_64-linux" ];  # save build time on the aarch64 builder
 })
