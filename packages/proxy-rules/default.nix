1f7f74015272a6f860535cc2ca6991c066d4489d
Gaoyang Zhang <gy@blurgy.xyz>
2024-06-29 13:31:27 +0800

    proxy-rules: clean up unused scripts
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- packages/proxy-rules/default.nix
+++ packages/proxy-rules/default.nix
@@ -1,15 +1,10 @@
 { generated, writeShellScript, stdenvNoCC
 , gnused
-, python3
 }:
 
 let
   rulesSrc = generated.v2ray-rules-dat.src;
   clashRulesBuildScript = import ./builders/clash.nix { inherit writeShellScript rulesSrc; };
-  pwp = python3.withPackages (pp: with pp; [
-    fastapi
-    uvicorn
-  ]);
 in
 
 stdenvNoCC.mkDerivation {
@@ -18,16 +13,11 @@ stdenvNoCC.mkDerivation {
 
   buildInputs = [
     gnused
-    pwp
   ];
 
   buildCommand = ''
     out=$TMPDIR/clash.yaml ${clashRulesBuildScript}
     install -Dvm444 $TMPDIR/clash.yaml $out/clash/generated.yaml
-
-    install -Dvm555 ${./src/clash-rules.py} $out/bin/clash-rules
-    install -Dvm555 ${./src/sing-box-rules.py} $out/bin/sing-box-rules
-    patchShebangs --build $out/bin/*
 
     install -Dvm644 -t $out/src/custom ${./custom-rules}/*.txt
     install -Dvm644 -t $out/src/upstream ${rulesSrc}/*.txt
