04e98862537aaf3cdff5ed44b9fab006ad54caf8
Gaoyang Zhang <gy@blurgy.xyz>
2024-07-14 12:07:32 +0800

    flake: remove non-existent input for rust-overlay
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- flake.nix
+++ flake.nix
@@ -25,10 +25,7 @@
     };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
