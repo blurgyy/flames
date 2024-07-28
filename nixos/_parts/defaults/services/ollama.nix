3a4325c20437e1c47df2f6249d12b5cdaf422028
Gaoyang Zhang <gy@blurgy.xyz>
2024-07-28 15:21:13 +0800

    nixos: remove obsolete `service.ollama.sandbox` option
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_parts/defaults/services/ollama.nix
+++ nixos/_parts/defaults/services/ollama.nix
@@ -5,8 +5,5 @@
     path = "/var/lib/${toString config.systemd.services.ollama.serviceConfig.StateDirectory}/.ollama/id_ed25519";
     sopsFile = ../../../_secrets.yaml;
   };
-  services.ollama = {
-    enable = true;
-    sandbox = false;  # disable sandboxing for it to be able to read ollama-privatekey
-  };
+  services.ollama.enable = true;
 }
