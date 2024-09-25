b7585017b3200ae44bea36fb4c45b7ddfdd8718a
Gaoyang Zhang <gy@blurgy.xyz>
2024-09-25 14:01:47 +0800

    module(sing-box-tailored): set route.default_interface instead of route.auto_detect_interface if router is enabled
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/route/default.nix
+++ nixos/_modules/sing-box-tailored/route/default.nix
@@ -1,10 +1,14 @@
-{ mapDir, applyTag, call }:
+{ config, mapDir, applyTag, call }:
 
 {
   rules = mapDir (path: call path {}) ./rules;
 
   rule_set = mapDir applyTag ./rule_set;
 
-  auto_detect_interface = true;
   final = "auto-oversea";
-}
+} // (if config.networking.nat.enable
+then {
+  default_interface = config.networking.nat.externalInterface;
+} else {
+  auto_detect_interface = true;
+})
