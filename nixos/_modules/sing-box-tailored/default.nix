3ec810574b5932b9f3996bfaac41de79d2405f33
Gaoyang Zhang <gy@blurgy.xyz>
2024-09-25 13:15:06 +0800

    module(sing-box-tailored): improve timeout logic for checking network responsiveness
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/default.nix
+++ nixos/_modules/sing-box-tailored/default.nix
@@ -95,8 +95,8 @@ in
             fi
 
             # Check network responsiveness
-            if ! ping -c 90 -i .99 8.8.8.8 &> /dev/null; then
-              echo "Network has been unresponsive for at least 1min30s.  Restarting wpa_supplicant."
+            if ! ping -c 90 -w 90 8.8.8.8 &> /dev/null; then
+              echo "Network has been unresponsive for 1min30s.  Restarting wpa_supplicant."
               systemctl restart wpa_supplicant-${iface}.service
               sleep 10  # Wait before continuing
               continue
