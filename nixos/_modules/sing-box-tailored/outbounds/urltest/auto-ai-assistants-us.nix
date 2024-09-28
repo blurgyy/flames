ca1dddfe6048068c662f40a1d605697dfe9446c4
Gaoyang Zhang <gy@blurgy.xyz>
2024-09-28 22:07:39 +0800

    nixos: update proxy rules
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/_modules/sing-box-tailored/outbounds/urltest/auto-ai-assistants-us.nix
+++ nixos/_modules/sing-box-tailored/outbounds/urltest/auto-ai-assistants-us.nix
@@ -3,6 +3,8 @@
   outbounds = [
     "us-00"
     "wss-us-00"
+    "us-02"
+    "wss-us-02"
   ];
   url = "https://www.gstatic.com/generate_204";
 }
--- nixos/_modules/sing-box-tailored/outbounds/urltest/auto-eu.nix
+++ nixos/_modules/sing-box-tailored/outbounds/urltest/auto-eu.nix
@@ -3,6 +3,8 @@
   outbounds = [
     "eu-02"
     "wss-eu-02"
+    "eu-03"
+    "wss-eu-03"
   ];
   url = "https://www.gstatic.com/generate_204";
 }
