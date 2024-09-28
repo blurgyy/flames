8769f5e413491100ff4d7c5cb5022374748af67e
Gaoyang Zhang <gy@blurgy.xyz>
2024-09-28 15:50:18 +0800

    tom: use simple imperative filesystem definitions
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- nixos/tom/configuration.nix
+++ nixos/tom/configuration.nix
@@ -3,36 +3,15 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.tmp.useTmpfs = false;
-  disko.devices.disk = {
-    main = {
-      device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_52761980";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            type = "EF00";
-            size = "512M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-              mountpoint = "/";
-              subvolumes = {
-                "/nix/store".mountpoint = "/nix/store";
-                "/var/lib/postgresql".mountpoint = "/var/lib/postgresql";
-              };
-            };
-          };
-        };
-      };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos-root";
+      fsType = "btrfs";
+      options = [ "noatime" "compress-force=zstd:3" "discard=async" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/nixos-boot";
+      fsType = "vfat";
     };
   };
 
