5a9fbd85737d1f547e6ebbb0ce4fb2efc2ded014
Gaoyang Zhang <gy@blurgy.xyz>
2024-09-29 13:54:30 +0800

    module(sing-box-tailored): fix missing outbound
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ nixos/_modules/sing-box-tailored/outbounds/remote/wss-eu-03.nix
@@ -0,0 +1,15 @@
+{ config }: {
+  type = "vmess";
+  # use domain as server for wss outbound
+  server._secret = config.sops.secrets."v2ray/domains/wss-eu-03".path;
+  server_port = 443;
+  security = "none";
+  tls = {
+    enabled = true;
+    server_name._secret = config.sops.secrets."v2ray/domains/wss-eu-03".path;
+  };
+  transport = {
+    type = "ws";
+    path._secret = config.sops.secrets."v2ray/ws-path".path;
+  };
+}
