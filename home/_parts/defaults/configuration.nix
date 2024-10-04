3f0b23bea11eaf1e0aee031dd815947445be847a
Gaoyang Zhang <gy@blurgy.xyz>
2024-10-05 02:33:17 +0800

    home: add radicle to environment packages
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- home/_parts/defaults/configuration.nix
+++ home/_parts/defaults/configuration.nix
@@ -45,6 +45,7 @@ in {
       procps
       procs
       progress
+      radicle-node
       ripgrep
       sdwrap
       telegram-send
@@ -228,6 +229,66 @@ in {
   };
 
   home.file = {
+    radiclePubkey = {
+      text = (import ../../../nixos/_parts/defaults/public-keys.nix).services."${myName}@radicle";
+      target = ".radicle/keys/radicle.pub";
+    };
+    radicleConfig = {
+      text = ''{
+        "publicExplorer": "https://git.blurgy.xyz/nodes/$host/$rid$path",
+        "preferredSeeds": [
+          "z6MkjXQGjLieiAijpGvX7kjkuEcmqQZK9MYA3oXtpLrqBbPD@radicle.blurgy.xyz:8776",
+          "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@seed.radicle.garden:8776",
+          "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@ash.radicle.garden:8776"
+        ],
+        "web": {
+          "pinned": {
+            "repositories": []
+          }
+        },
+        "cli": {
+          "hints": true
+        },
+        "node": {
+          "alias": "highsunz",
+          "listen": [],
+          "peers": {
+            "type": "dynamic"
+          },
+          "connect": [],
+          "externalAddresses": [],
+          "network": "main",
+          "log": "INFO",
+          "relay": "auto",
+          "limits": {
+            "routingMaxSize": 1000,
+            "routingMaxAge": 604800,
+            "gossipMaxAge": 1209600,
+            "fetchConcurrency": 1,
+            "maxOpenFiles": 4096,
+            "rate": {
+              "inbound": {
+                "fillRate": 5.0,
+                "capacity": 1024
+              },
+              "outbound": {
+                "fillRate": 10.0,
+                "capacity": 2048
+              }
+            },
+            "connection": {
+              "inbound": 128,
+              "outbound": 16
+            }
+          },
+          "workers": 8,
+          "seedingPolicy": {
+            "default": "block"
+          }
+        }
+      }'';
+      target = ".radicle/config.json";
+    };
     ipythonConfig = {
       text = ''
         c.TerminalInteractiveShell.editing_mode = "vi"
